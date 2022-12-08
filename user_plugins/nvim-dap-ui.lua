9e58511aec6be410c28c64754973e8f436f49e21
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2022-12-08 12:33:20 +0600

    feat(user_plugins): Enabled auto exit dap-ui on DAP exit/termination

--- user_plugins/nvim-dap-ui.lua
+++ user_plugins/nvim-dap-ui.lua
@@ -2,6 +2,6 @@ return function()
     local dap, dapui = require("dap"), require("dapui")
     dapui.setup()
     dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
-    -- dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-    -- dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
 end
