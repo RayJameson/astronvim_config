85b590593871da238b0ee123975ba40876dd3b4c
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2022-12-09 23:29:20 +0600

    feat(keymaps): Swapped go to definition to Trouble insted of Telescope

--- lsp/mappings.lua
+++ lsp/mappings.lua
@@ -1,7 +1,8 @@
 return {
     -- easily add or disable built in mappings added during LSP attaching
     n = {
-        -- ["<leader>lf"] = false -- disable formatting keymap
         ["<leader>lF"] = { "<cmd>FormatModifications<CR>", desc = "Format changed code" },
+        ["gr"] = { "<cmd>Trouble lsp_references<CR>", desc = "References" },
+        ["gd"] = { "<cmd>Trouble lsp_definitions<CR>", desc = "Definitions" },
     },
 }
