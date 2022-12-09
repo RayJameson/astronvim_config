06d5e2929359afcff32fd00e6e659a2cea6235f2
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2022-12-09 23:35:18 +0600

    fix(lazy_loading): Fixed treesitter-context lazy loading

--- plugins/init.lua
+++ plugins/init.lua
@@ -89,6 +89,7 @@ return {
     },
     ["nvim-treesitter/nvim-treesitter-context"] = {
         after = "nvim-treesitter",
+        config = function() require("treesitter-context").setup() end,
     },
     ["nvim-treesitter/nvim-treesitter-textobjects"] = {
         after = "nvim-treesitter",
