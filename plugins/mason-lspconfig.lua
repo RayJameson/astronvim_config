b0655bcb2f035a717fdc29ae088ab9118ca20ad1
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2022-12-11 15:08:54 +0600

    feat(lsp): Added more lsp servers to ensure installed

--- plugins/mason-lspconfig.lua
+++ plugins/mason-lspconfig.lua
@@ -1,4 +1,16 @@
 return { -- overrides `require("mason-lspconfig").setup(...)`
-    ensure_installed = { "sumneko_lua", "pylsp" },
+    ensure_installed = {
+        "sumneko_lua",
+        "pylsp",
+        "rust_analyzer",
+        "html",
+        "cssls",
+        "vimls",
+        "yamlls",
+        "dockerls",
+        "jsonls",
+        "taplo",
+        "cssmodules_ls",
+    },
     automatic_installation = true,
 }
