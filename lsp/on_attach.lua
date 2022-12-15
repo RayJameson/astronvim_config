5a9d09bc569d7418455dd9d163857f26d3435b2f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2022-12-15 12:10:13 +0600

    fix(lsp): Fix for lsp-format-modification plugin
    Fixed error msg when lsp doen't support range formatting

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -1,6 +1,8 @@
 return function(client, bufnr)
+    if client.server_capabilities.DocumentRangeFormattingProvider then
     local lsp_format_modifications = require("lsp-format-modifications")
     lsp_format_modifications.attach(client, bufnr, { format_on_save = false })
+    end
 
     if client.server_capabilities.inlayHintProvider then
         local inlayhints_avail, inlayhints = pcall(require, "lsp-inlayhints")
