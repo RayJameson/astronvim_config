bdbedb90cf9049190c428a719b2ed1110e63bbeb
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2022-12-15 11:56:03 +0600

    refactor(keymaps): Moved keymaps from which-key to mappings file

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,13 @@
 local is_available = astronvim.is_available
 local keymaps = { n = {}, c = {}, i = {}, v = {}, t = {} }
 
+keymaps.n["<leader>y"] = { '"+y', desc = "yank +clipboard" }
+keymaps.n["<leader>Y"] = { '"+y$', desc = "Yank +clipboard" }
+keymaps.v["<leader>y"] = { '"+y', desc = "yank +clipboard" }
+keymaps.v["<leader>Y"] = { '"+y$', desc = "Yank +clipboard" }
+keymaps.n["<leader>d"] = { '"_d', desc = "Delete noregister" }
+keymaps.v["<leader>d"] = { '"_d', desc = "Delete noregister" }
+keymaps.v["<leader>p"] = { '"_dP', desc = "Paste noregister" }
 keymaps.n["<leader>."] = { ":cd %:p:h<CR>", desc = "CD to current file" }
 keymaps.n["<leader>ll"] = { "<cmd>LinterRestart<CR>", desc = "Linter restart" }
 keymaps.n["<leader>bb"] = { "<cmd>tabnew<CR>", desc = "New tab" }
@@ -20,12 +27,56 @@ keymaps.i["<C-j>"] = { "<Down>", desc = "Down" }
 keymaps.i["<C-k>"] = { "<Up>", desc = "Up" }
 keymaps.i["<C-l>"] = { "<Right>", desc = "Right" }
 
+if is_available("refactoring.nvim") then
+    keymaps.v["<leader>r"] = { "<cmd>Refactoring<CR>", desc = "Refactoring" }
+end
+
 if is_available("diffview.nvim") then
     keymaps.n["<leader>gd"] = { "<cmd>DiffviewOpen<CR>", desc = "Open diff view (g? help)", silent = true }
     keymaps.n["<leader>gc"] = { "<cmd>DiffviewClose<CR>", desc = "Close diff view" }
     keymaps.n["<leader>gh"] = { "<cmd>DiffviewFileHistory %<CR>", desc = "Open file history", silent = true }
     keymaps.n["<leader>gH"] = { "<cmd>DiffviewFileHistory<CR>", desc = "Open branch history", silent = true }
     keymaps.v["<leader>gh"] = { ":DiffviewFileHistory<CR>", desc = "Open line history", silent = true }
+end
+
+if is_available("code_runner.nvim") then
+    keymaps.n["<leader>rr"] = { "<cmd>RunCode<CR>", desc = "Run code" }
+    keymaps.n["<leader>rf"] = { "<cmd>RunFile<CR>", desc = "Run file" }
+    keymaps.n["<leader>rt"] = { "<cmd>RunFile tab<CR>", desc = "Run file tab" }
+    keymaps.n["<leader>rb"] = { "<cmd>RunFile buf<CR>", desc = "Run file buf" }
+    keymaps.n["<leader>rc"] = { "<cmd>RunClose<CR>", desc = "Close runner" }
+end
+
+if is_available("trouble.nvim") then
+    keymaps.n["<leader>Tr"] = { "<cmd>Trouble lsp_references<CR>", desc = "References" }
+    keymaps.n["<leader>Tf"] = { "<cmd>Trouble lsp_definitions<CR>", desc = "Definitions" }
+    keymaps.n["<leader>Td"] = { "<cmd>Trouble document_diagnostics<CR>", desc = "Diagnostics" }
+    keymaps.n["<leader>Tq"] = { "<cmd>Trouble quickfix<CR>", desc = "QuickFix" }
+    keymaps.n["<leader>Tl"] = { "<cmd>Trouble loclist<CR>", desc = "LocationList" }
+    keymaps.n["<leader>Tw"] = { "<cmd>Trouble workspace_diagnostics<CR>", desc = "Wordspace Diagnostics" }
+    keymaps.n["<leader>Tt"] = { "<cmd>TodoTrouble<CR>", desc = "TODO list" }
+end
+
+if is_available("glow.nvim") then
+    keymaps.n["<leader>mM"] = { "<cmd>Glow<CR>", desc = "Markdown Glow" }
+end
+
+if is_available("nvim-dap") then
+    keymaps.n["<leader>Db"] = { "<cmd>lua require'dap'.toggle_breakpoint()<CR>", desc = "Breakpoint (F9)" }
+    keymaps.n["<leader>Dc"] = { "<cmd>lua require'dap'.continue()<CR>", desc = "Continue" }
+    keymaps.n["<leader>Di"] = { "<cmd>lua require'dap'.step_into()<CR>", desc = "Into" }
+    keymaps.n["<leader>Do"] = { "<cmd>lua require'dap'.step_over()<CR>", desc = "Over" }
+    keymaps.n["<leader>DO"] = { "<cmd>lua require'dap'.step_out()<CR>", desc = "Out" }
+    keymaps.n["<leader>Dr"] = { "<cmd>lua require'dap'.repl.toggle()<CR>", desc = "Repl" }
+    keymaps.n["<leader>Dl"] = { "<cmd>lua require'dap'.run_last()<CR>", desc = "Last" }
+    keymaps.n["<leader>Du"] = { "<cmd>lua require'dapui'.toggle()<CR>", desc = "UI" }
+    keymaps.n["<leader>Dx"] = { "<cmd>lua require'dap'.terminate()<CR>", desc = "Exit" }
+end
+
+if is_available("markdown-preview.nvim") then
+    keymaps.n["<leader>mm"] = { "<cmd>MarkdownPreview<CR>", desc = "MarkdownPreview" }
+    keymaps.n["<leader>mt"] = { "<cmd>MarkdownPreviewToggle<CR>", desc = "MarkdownPreview Toggle" }
+    keymaps.n["<leader>ms"] = { "<cmd>MarkdownPreviewStop<CR>", desc = "MarkdownPreview Stop" }
 end
 
 if is_available("undotree") then
--- which-key/register.lua
+++ which-key/register.lua
@@ -5,64 +5,19 @@ return {
         ["<leader>"] = {
             -- third key is the key to bring up next level and its displayed
             -- group name in which-key top level menu
-            ["y"] = { '"+y', "yank +clipboard" },
-            ["Y"] = { '"+y$', "Yank +clipboard" },
-            ["d"] = { '"_d', "delete noregister" },
             ["b"] = { name = "Buffer" },
-            ["m"] = {
-                name = "Markdown",
-                ["M"] = { "<cmd>Glow<CR>", "Markdown Glow" },
-                ["m"] = { "<cmd>MarkdownPreview<CR>", "MarkdownPreview" },
-                ["t"] = { "<cmd>MarkdownPreviewToggle<CR>", "MarkdownPreview Toggle" },
-                ["s"] = { "<cmd>MarkdownPreviewStop<CR>", "MarkdownPreview Stop" },
-            },
+            ["m"] = { name = "Markdown" },
             ["r"] = {
                 name = "Code runner",
-                ["r"] = { "<cmd>RunCode<CR>", "Run code" },
-                ["f"] = { "<cmd>RunFile<CR>", "Run file" },
-                ["t"] = { "<cmd>RunFile tab<CR>", "Run file tab" },
-                ["b"] = { "<cmd>RunFile buf<CR>", "Run file buf" },
-                ["c"] = { "<cmd>RunClose<CR>", "Close runner" },
             },
-            ["T"] = {
-                name = "Trouble",
-                ["r"] = { "<cmd>Trouble lsp_references<CR>", "References" },
-                ["f"] = { "<cmd>Trouble lsp_definitions<CR>", "Definitions" },
-                ["d"] = { "<cmd>Trouble document_diagnostics<CR>", "Diagnostics" },
-                ["q"] = { "<cmd>Trouble quickfix<CR>", "QuickFix" },
-                ["l"] = { "<cmd>Trouble loclist<CR>", "LocationList" },
-                ["w"] = {
-                    "<cmd>Trouble workspace_diagnostics<CR>",
-                    "Wordspace Diagnostics",
-                },
-                ["t"] = { "<cmd>TodoTrouble<CR>", "TODO list" },
-            },
-            ["D"] = {
-                name = "Debug",
-                ["b"] = { "<cmd>lua require'dap'.toggle_breakpoint()<CR>", "Breakpoint" },
-                ["c"] = { "<cmd>lua require'dap'.continue()<CR>", "Continue" },
-                ["i"] = { "<cmd>lua require'dap'.step_into()<CR>", "Into" },
-                ["o"] = { "<cmd>lua require'dap'.step_over()<CR>", "Over" },
-                ["O"] = { "<cmd>lua require'dap'.step_out()<CR>", "Out" },
-                ["r"] = { "<cmd>lua require'dap'.repl.toggle()<CR>", "Repl" },
-                ["l"] = { "<cmd>lua require'dap'.run_last()<CR>", "Last" },
-                ["u"] = { "<cmd>lua require'dapui'.toggle()<CR>", "UI" },
-                ["x"] = { "<cmd>lua require'dap'.terminate()<CR>", "Exit" },
-            },
+            ["T"] = { name = "Trouble" },
+            ["D"] = { name = "Debug" },
         },
     },
     v = {
         ["<leader>"] = {
-            ["y"] = { '"+y', "yank +clipboard" },
-            ["Y"] = { '"+y', "Yank +clipboard" },
-            ["d"] = { '"_d', "delete noregister" },
-            ["D"] = { '"_D', "Delete noregister" },
-            ["r"] = { "<Esc><cmd>Refactoring<CR>", "Refactoring" },
-        },
-    },
-    x = {
-        ["<leader>"] = {
-            ["p"] = { '"_dP', "Paste noregister" },
+            ["g"] = { name = "Diff View" },
+            ["l"] = { name = "LSP" },
         },
     },
 }
