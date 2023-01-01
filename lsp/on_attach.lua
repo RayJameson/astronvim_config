5c62d905384bfbe9a5734375cb8e892a8eaf8403
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-02 03:50:14 +0600

    feat(on_attach): Safe require plugins

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -1,14 +1,17 @@
 return function(client, bufnr)
     if client.server_capabilities.DocumentRangeFormattingProvider then
-    local lsp_format_modifications = require("lsp-format-modifications")
-    lsp_format_modifications.attach(client, bufnr, { format_on_save = false })
+        local is_available, lsp_format_modifications = pcall(require, "lsp-format-modifications")
+        if is_available then
+            lsp_format_modifications.attach(client, bufnr, { format_on_save = false })
+        end
     end
 
     if client.server_capabilities.inlayHintProvider then
-        local inlayhints_avail, inlayhints = pcall(require, "lsp-inlayhints")
-        if inlayhints_avail then
+        local is_available, inlayhints = pcall(require, "lsp-inlayhints")
+        if is_available then
             inlayhints.on_attach(client, bufnr)
             vim.keymap.set("n", "<leader>uH", function() inlayhints.toggle() end, { desc = "Toggle inlay hints" })
+            vim.cmd("hi! link LspInlayHint Comment")
         end
     end
 end
