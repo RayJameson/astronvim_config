cce19a01d35bf896f2b0538bf19a9076533a71bc
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-02 03:49:16 +0600

    feat(mason-lspconfig): Removed css from mandatory

--- plugins/mason-lspconfig.lua
+++ plugins/mason-lspconfig.lua
@@ -4,13 +4,11 @@ return { -- overrides `require("mason-lspconfig").setup(...)`
         "pylsp",
         "rust_analyzer",
         "html",
-        "cssls",
         "vimls",
         "yamlls",
         "dockerls",
         "jsonls",
         "taplo",
-        "cssmodules_ls",
     },
     automatic_installation = true,
 }
