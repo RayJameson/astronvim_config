fc501d287c14c3f04233e6659a128ded2b057122
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-02 04:20:09 +0600

    feat: I don't remember why i added that

--- polish.lua
+++ polish.lua
@@ -48,6 +48,12 @@ return function()
         end,
     })
 
+    vim.lsp.handlers["workspace/diagnostic/refresh"] = function(_, _, ctx)
+        local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
+        pcall(vim.diagnostic.reset, ns)
+        return true
+    end
+
     vim.filetype.add {
         filename = {
             ["poetry.lock"] = "toml",
