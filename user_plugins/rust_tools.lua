4831d22fa163cb0c6f7fb62cbf2a3ff521125e8b
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-02 03:58:55 +0600

    feat(rust_tools): Disable builtin inlay hints in favor of plugin's
    plugin: lsp-inlayhints

--- user_plugins/rust_tools.lua
+++ user_plugins/rust_tools.lua
@@ -1,8 +1,8 @@
 return function()
     require("rust-tools").setup {
         tools = {
-            on_initialized = function() require("inlay-hints").set_all() end,
-            inlay_hints = { auto = false },
+            -- on_initialized = function() require("inlay-hints").set_all() end,
+            -- inlay_hints = { auto = false },
         },
         server = astronvim.lsp.server_settings("rust_analyzer"), -- get the server settings and built in capabilities/on_attach
     }
