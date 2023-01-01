359d8810646d8d0fcbec5a3b77cfbf12ad65c3d2
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-02 03:44:18 +0600

    fix(treesitter): Correct option to disable ts highlighting

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -78,11 +78,13 @@ return {
             enable = false,
         },
     },
-    disable = function(lang, buf)
-        local max_filesize = 500 * 1024 -- 500 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
+    highlight = {
+        disable = function(lang, buf)
+            local max_filesize = 500 * 1024 -- 500 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+    },
 }
