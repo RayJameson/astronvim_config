76d118d51fc93a1b75e60e503dcc97e0395d2c6b
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-04 02:43:49 +0600

    feat(polish): autocmd to drop snippet completion on mode change

--- polish.lua
+++ polish.lua
@@ -83,4 +83,20 @@ return function()
             vim.api.nvim_win_set_option(0, "foldcolumn", "auto:9")
         end,
     })
+
+    local function leave_snippet()
+        if
+            ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+            and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not require("luasnip").session.jump_active
+        then
+            require("luasnip").unlink_current()
+        end
+    end
+
+    -- stop snippets when you leave to normal mode
+    vim.api.nvim_create_autocmd("ModeChanged", {
+        pattern = "*",
+        callback = function() leave_snippet() end,
+    })
 end
