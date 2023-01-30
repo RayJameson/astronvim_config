529b3c298240316b4da31a94cc638a576fb2ae59
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-31 03:40:11 +0600

    refactor(plugins): Restore original repo for move.nvim plugin

--- plugins/init.lua
+++ plugins/init.lua
@@ -111,7 +111,7 @@ return {
         opt = true,
         setup = add_to_lazy_file_plugins("undotree"),
     },
-    ["Fau818/move.nvim"] = {
+    ["fedepujol/move.nvim"] = {
         opt = true,
         setup = add_to_lazy_file_plugins("move.nvim"),
     },
