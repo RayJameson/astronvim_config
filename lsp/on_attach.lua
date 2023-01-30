01963b374e8b38df03654f12e084f6cb214573ff
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-01-31 03:49:44 +0600

    feat(lsp): disable all capabilities except autocompletion for pyright

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -21,4 +21,18 @@ return function(client, bufnr)
     if client.name == "ruff_lsp" then
         client.server_capabilities.hoverProvider = false
     end
+    if client.name == "pyright" then
+        local enabled_capabilities = {
+            textDocumentSync = true,
+            completionProvider = true,
+            documentHighlightProvider = true,
+            documentSymbolProvider = true,
+            workspaceSymbolProvider = true,
+        }
+        for capability_name, _ in pairs(client.server_capabilities) do
+            if not enabled_capabilities[capability_name] then
+                client.server_capabilities[capability_name] = false
+            end
+        end
+    end
 end
