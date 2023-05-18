aab67d48fa6c8e60cd79f4b6223a0d6cef3a0d4e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-05-18 13:42:53 +0600

    feat(user): add ai completion plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -188,4 +188,23 @@ return {
       paint_arg_usages = true,
     },
   },
+  {
+    "Exafunction/codeium.vim",
+    event = "User Astrofile",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<C-n>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-p>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
 }
