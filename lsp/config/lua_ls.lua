561e36c4fc5f225b6a5b606c7304d8328102b97f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-05-18 11:00:50 +0600

    fix(lua_ls): migrate to new lsp name

---
+++ lsp/config/lua_ls.lua
@@ -0,0 +1,25 @@
+return {
+  settings = {
+    Lua = {
+      format = {
+        enable = false,
+      },
+      hint = {
+        enable = true,
+        setType = true,
+      },
+      workspace = {
+        checkThirdParty = false,
+      },
+      diagnostics = {
+        disable = { "lowercase-global" },
+      },
+      codelens = {
+        enable = true,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
