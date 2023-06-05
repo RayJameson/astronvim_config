7179f35e3c8ee8f4da196e5760f5b26896f2b50f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-05 19:10:05 +0600

    feat(treesitter): add most used parsers to ensure installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,8 +2,25 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
-    })
+    opts.ensure_installed =
+      require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "lua",
+        "luadoc",
+        "python",
+        "rust",
+        "vim",
+        "markdown",
+        "make",
+        "json",
+        "json5",
+        "yaml",
+        "toml",
+        "bash",
+        "gitcommit",
+        "gitattributes",
+        "gitignore",
+        "git_config",
+        "git_rebase",
+      })
   end,
 }
