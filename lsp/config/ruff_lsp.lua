58156dac13fb32c29e006cffe534740d87a2e6c8
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-16 19:28:34 +0600

    chore(lsp): remove PD008 rule in ruff because it's misleading
    https://github.com/deppen8/pandas-vet/issues/105

--- lsp/config/ruff_lsp.lua
+++ lsp/config/ruff_lsp.lua
@@ -3,7 +3,7 @@ return {
     settings = {
       args = {
         "--select=ALL",
-        "--ignore=D,PT009,ANN002,ANN003,ANN101,ANN102,ANN401,A003,RUF001,RUF002,RUF003,RET504",
+        "--ignore=D,PT009,ANN002,ANN003,ANN101,ANN102,ANN401,A003,RUF001,RUF002,RUF003,RET504,PD008",
         "--line-length=79",
         "--unfixable=F401",
       },
