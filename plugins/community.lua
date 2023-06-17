f69e58bbfbf8571accae11afda020a5de1307651
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-18 02:37:11 +0600

    style(formatting): move lines for consistency

--- plugins/community.lua
+++ plugins/community.lua
@@ -18,8 +18,8 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.utility.transparent-nvim" },
   { import = "astrocommunity.utility.neodim" },
+  { import = "astrocommunity.utility.transparent-nvim" },
   {
     "transparent.nvim",
     priority = 1000,
