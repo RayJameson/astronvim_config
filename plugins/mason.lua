26f31704577b30bd23464d94d874b35b719db670
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-20 12:59:04 +0600

    feat(mason): add dap configs for python

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,3 +1,4 @@
+local list_insert_unique = require("astronvim.utils").list_insert_unique
 -- customize mason plugins
 return {
   -- use mason-lspconfig to configure LSP installations
@@ -6,10 +7,9 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed =
-        require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-          -- "lua_ls",
-        })
+      opts.ensure_installed = list_insert_unique(opts.ensure_installed, {
+        -- "lua_ls",
+      })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -18,11 +18,10 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed =
-        require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-          -- "prettier",
-          -- "stylua",
-        })
+      opts.ensure_installed = list_insert_unique(opts.ensure_installed, {
+        -- "prettier",
+        -- "stylua",
+      })
     end,
   },
   {
@@ -30,10 +29,49 @@ return {
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed =
-        require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-          -- "python",
-        })
+      opts.ensure_installed = list_insert_unique(opts.ensure_installed, {
+        -- "python",
+      })
+      opts.handlers = {
+        python = function(config)
+          config.adapters = {
+            type = "executable",
+            command = "python3",
+            args = {
+              "-m",
+              "debugpy.adapter",
+            },
+          }
+          config.configurations = {
+            {
+              type = "python",
+              request = "launch",
+              name = "Python: Debug Current File",
+              program = "${file}",
+              justMyCode = false,
+              console = "integratedTerminal",
+              pythonPath = function()
+                return "python"
+              end,
+            },
+            {
+              type = "python",
+              request = "launch",
+              name = "Python: Debug FastAPI web application",
+              module = "uvicorn",
+              args = {
+                "main:app",
+                "--use-colors",
+              },
+              pythonPath = function()
+                return "python"
+              end,
+              console = "integratedTerminal",
+            },
+          }
+          require("mason-nvim-dap").default_setup(config) -- don't forget this!
+        end,
+      }
     end,
   },
 }
