f95c56865c2935b56590402d5433a1ea18b4dc8c
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-22 09:26:07 +0600

    chore(options): remove unused options

--- options.lua
+++ options.lua
@@ -50,8 +50,6 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    undotree_setfocuswhentoggle = true,
-    undotree_splitwidth = 40,
   },
 }
 -- If you need more control, you can use the function()...end notation
