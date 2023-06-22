217ca26a36d1a11681091f50a1099dd5e93cf559
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-22 17:27:23 +0600

    chore(lsp): ignore unwanted warnings from ruff_lsp

--- lsp/config/ruff_lsp.lua
+++ lsp/config/ruff_lsp.lua
@@ -3,7 +3,7 @@ return {
     settings = {
       args = {
         "--select=ALL",
-        "--ignore=D,PT009,ANN002,ANN003,ANN101,ANN102,ANN401,A003,RUF001,RUF002,RUF003,RET504,PD008",
+        "--ignore=D,PT009,ANN002,ANN003,ANN101,ANN102,ANN401,A003,RUF001,RUF002,RUF003,RET504,PD008,TD001,TD002,TD003,S101",
         "--line-length=79",
         "--unfixable=F401",
       },
