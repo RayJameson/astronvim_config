d6d5a2ac55b958572ce49cdd1322c950944f653b
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-30 13:30:22 +0600

    feat(plugins): trying oil.nvim instead of neo-tree

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  enabled = false,
   opts = {
     event_handlers = {
       {
--- plugins/user.lua
+++ plugins/user.lua
@@ -402,4 +402,48 @@ return {
       vim.keymap.set("n", "yss", "ys_", { remap = true })
     end,
   },
+  {
+    "stevearc/oil.nvim",
+    config = true,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "-",
+        function()
+          return require("oil").open()
+        end,
+        desc = "Open parent directory",
+      },
+    },
+    init = function()
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        -- Capture the protocol and lazy load oil if it is "oil-ssh", besides also lazy
+        -- loading it when the first argument is a directory.
+        local adapter = string.match(vim.fn.argv(0), "^([%l-]*)://")
+        if (stat and stat.type == "directory") or adapter == "oil-ssh" then
+          require("lazy").load { plugins = { "oil.nvim" } }
+        end
+      end
+      if not require("lazy.core.config").plugins["oil.nvim"]._.loaded then
+        vim.api.nvim_create_autocmd("BufNew", {
+          callback = function()
+            if vim.fn.isdirectory(vim.fn.expand("<afile>")) == 1 then
+              require("lazy").load { plugins = { "oil.nvim" } }
+              -- Once oil is loaded, we can delete this autocmd
+              return true
+            end
+          end,
+        })
+      end
+    end,
+    opts = {
+      skip_confirm_for_simple_edits = true,
+      delete_to_trash = true,
+      trash_command = "trash",
+      view_options = {
+        show_hidden = true,
+      },
+    },
+  },
 }
