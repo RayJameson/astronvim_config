d659c0bc645eeec001dff44c20eae404e6df2d10
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-04 14:45:55 +0600

    feat(lsp): add metals (Scala LS)

--- plugins/user.lua
+++ plugins/user.lua
@@ -478,4 +478,22 @@ return {
       },
     },
   },
+  {
+    "scalameta/nvim-metals",
+    dependencies = "nvim-lua/plenary.nvim",
+    init = function()
+      vim.env.PATH = vim.env.PATH
+        .. ":"
+        .. vim.fn.expand("$HOME/.cache/nvim/nvim-metals")
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(
+            require("astronvim.utils.lsp").config("metals")
+          )
+        end,
+        group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }),
+      })
+    end,
+  },
 }
