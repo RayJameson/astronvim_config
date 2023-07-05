1d7638c97eadf094d466c13e15f4d6184d058564
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-06 05:14:46 +0600

    feat(polish): crazy vim magic to execute macros on search res

--- polish.lua
+++ polish.lua
@@ -34,6 +34,45 @@ return function()
     pcall(vim.diagnostic.reset, ns)
     return true
   end
+  -----------------------------------------------------------------------------//
+  -- Absolutely fantastic function from stoeffel/.dotfiles which allows you to
+  -- repeat macros across a visual range
+  ------------------------------------------------------------------------------
+  -- TODO: converting this to lua does not work for some obscure reason.
+  vim.cmd([[
+    function! ExecuteMacroOverVisualRange()
+      execute ":'<,'>normal @".nr2char(getchar())
+    endfunction
+  ]])
+  -----------------------------------------------------------------------------//
+  -- Multiple Cursor Replacement
+  -- http://www.kevinli.co/posts/2017-01-19-multiple-cursors-in-500-bytes-of-vimscript/
+  -----------------------------------------------------------------------------//
+
+  -- 1. Position the cursor over a word; alternatively, make a selection.
+  -- 2. Hit cq to start recording the macro.
+  -- 3. Once you are done with the macro, go back to normal mode.
+  -- 4. Hit Enter to repeat the macro over search matches.
+
+  vim.cmd([[
+  let g:mc = "y/\\V\<C-r>=escape(@\", '/')\<CR>\<CR>"
+
+  nnoremap cn *``cgn
+  nnoremap cN *``cgN
+
+  vnoremap <expr> cn g:mc . "``cgn"
+  vnoremap <expr> cN g:mc . "``cgN"
+
+  function! SetupCR()
+    nnoremap <Enter> :nnoremap <lt>Enter> n@z<CR>q:<C-u>let @z=strpart(@z,0,strlen(@z)-1)<CR>n@z
+  endfunction
+
+  nnoremap cq :call SetupCR()<CR>*``qz
+  nnoremap cQ :call SetupCR()<CR>#``qz
+
+  vnoremap <expr> cq ":\<C-u>call SetupCR()\<CR>" . "gv" . g:mc . "``qz"
+  vnoremap <expr> cQ ":\<C-u>call SetupCR()\<CR>" . "gv" . substitute(g:mc, '/', '?', 'g') . "``qz"
+]])
 
   -- Set up custom filetypes
   vim.filetype.add {
