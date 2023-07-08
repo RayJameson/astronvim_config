c8451690c8544820b5aa730b74cd39709c109c15
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-08 11:16:03 +0600

    feat(lsp): disable `jedi_language_server`'s formatting capability'

--- lsp/formatting.lua
+++ lsp/formatting.lua
@@ -12,6 +12,7 @@ return {
   disabled = { -- disable formatting capabilities for the listed language servers
     -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
     "lua_ls",
+    "jedi_language_server",
   },
   timeout_ms = 1000, -- default format timeout
   filter = function(client)
