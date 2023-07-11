c4423536fc65fb2307e292d3490b639f9a749462
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-11 10:34:19 +0600

    fix(astrotheme): pin astrotheme version

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,6 +1,11 @@
 return {
   -- customize alpha options
   {
+    "AstroNvim/astrotheme",
+    commit = "7a52efdd9a5bd302445d284a424467f92e4b1d44",
+    optional = true,
+  },
+  {
     "goolord/alpha-nvim",
     opts = function(_, opts)
       -- customize the dashboard header
