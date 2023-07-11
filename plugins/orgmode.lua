01cc7b2910d2699a9830db87c89bb2746339d7fb
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-11 10:33:38 +0600

    feat(orgmode): add more templates

--- plugins/orgmode.lua
+++ plugins/orgmode.lua
@@ -1,3 +1,33 @@
+---Return `date` with Monday as first day of week
+---@param date table
+---@return osdate
+local function monday_start(date)
+  date.wday = date.wday == 1 and 7 or date.wday - 1
+  return date
+end
+
+local get_week_nr = function()
+  local date = os.date("*t")
+  date.wday = date.wday == 1 and 7 or date.wday - 1
+  local week_map = {}
+  local current_week = 1
+  for i = 1, date.day do
+    local i_date = monday_start(
+      os.date("*t", os.time { year = date.year, month = date.month, day = i })
+    )
+    week_map[i] = current_week
+    if i_date.wday == 7 then
+      current_week = current_week + 1
+    end
+  end
+  date.wnum = week_map[date.day]
+  return tostring(date.wnum)
+end
+
+local current_week = function()
+  return os.date("%Y/%m/") .. "W" .. get_week_nr()
+end
+
 return {
   "nvim-orgmode/orgmode",
   event = "VeryLazy",
@@ -27,7 +57,7 @@ return {
       end,
     })
     vim.api.nvim_create_autocmd({ "FileType" }, {
-      pattern = "org",
+      pattern = { "org", "orgagenda" },
       group = "OrgSetup",
       callback = function()
         vim.wo.conceallevel = 2
@@ -43,7 +73,6 @@ return {
     return {
       org_agenda_files = org_path("**/*"),
       org_default_notes_file = org_path("refile.org"),
-      org_agenda_start_on_weekday = false,
       org_todo_keywords = {
         "TODO(t)",
         "PROGRESS(p)",
@@ -57,19 +86,43 @@ return {
         },
       },
       org_capture_templates = {
-        t = {
+        r = {
           description = "Refile",
           template = "* TODO %?\n  DEADLINE: %T",
         },
-        T = {
+        t = {
           description = "Todo (no category)",
           template = "* TODO %?\n  DEADLINE: %T",
           target = org_path("todos.org"),
         },
         w = {
-          description = "Work todo",
+          description = "Work week todo",
           template = "* TODO %?\n  DEADLINE: %T",
-          target = org_path("work.org"),
+          target = org_path("work/" .. current_week() .. "/work.org"),
+        },
+        j = {
+          description = "Journal",
+          template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+          target = org_path("journal.org"),
+        },
+        e = "Event",
+        er = {
+          description = "Recurring",
+          template = "** %?\n %T",
+          target = org_path("calendar.org"),
+          headline = "Recurring",
+        },
+        eo = {
+          description = "One time",
+          template = "** %?\n %T",
+          target = org_path("calendar.org"),
+          headline = "One time",
+        },
+        eb = {
+          description = "Birthdays",
+          template = "** %?\n %T",
+          target = org_path("calendar.org"),
+          headline = "Birthdays",
         },
       },
     }
