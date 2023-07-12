c1e5f0729cccac6653292ab086e0c9d2329e2faa
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-12 16:16:15 +0600

    feat(lsp): disable telescope integration for lsp mappings

--- lsp/mappings.lua
+++ lsp/mappings.lua
@@ -1,4 +1,10 @@
-return function(mappings)
-  mappings.n["gl"] = false
-  return mappings
+return function(maps)
+  maps.n["gl"] = false
+  if maps.n.gd then maps.n.gd[1] = function() vim.lsp.buf.definition() end end
+  if maps.n.gI then maps.n.gI[1] = function() vim.lsp.buf.implementation() end end
+  if maps.n.gr then maps.n.gr[1] = function() vim.lsp.buf.references() end end
+  if maps.n.gT then maps.n.gT[1] = function() vim.lsp.buf.type_definition() end end
+  if maps.n["<leader>lG"] then maps.n["<leader>lG"][1] = function() vim.lsp.buf.workspace_symbol() end end
+  if maps.n["<leader>lR"] then maps.n["<leader>lR"][1] = function() vim.lsp.buf.references() end end
+  return maps
 end
