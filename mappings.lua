9af4b553126fbfcce4dba70fd76c5bf9e4dce4cc
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-12 11:29:12 +0600

    style(mappings): change formatting

--- mappings.lua
+++ mappings.lua
@@ -86,12 +86,7 @@ maps.v["gk"] = { "gg", desc = "go to first line" }
 --]
 
 if is_available("nvim-notify") then
-  maps.n["<leader>uD"] = {
-    function()
-      require("notify").dismiss { silent = true }
-    end,
-    desc = "Dismiss notification",
-  }
+  maps.n["<leader>uD"] = { function() require("notify").dismiss { silent = true } end, desc = "Dismiss notification", }
 end
 
 if is_available("orgmode") then
@@ -122,29 +117,21 @@ if is_available("mini.ai") then
 end
 
 if is_available("telescope.nvim") then
-  maps.n["<leader>fg"] = {
-    require("telescope.builtin").git_files,
-    desc = "Find git files",
-  }
-  maps.n["<leader>fs"] = {
-    require("telescope.builtin").spell_suggest,
-    desc = "Show spell suggestions",
-  }
+  maps.n["<leader>fg"] = { require("telescope.builtin").git_files, desc = "Find git files", }
+  maps.n["<leader>fs"] = { require("telescope.builtin").spell_suggest, desc = "Show spell suggestions", }
   local telescope_undo_available, _ = pcall(require, "telescope-undo")
   if telescope_undo_available then
-    maps.n["<leader>fu"] = {
-      require("telescope").extensions.undo.undo,
-      desc = "Show undo history",
-    }
+    maps.n["<leader>fu"] = { require("telescope").extensions.undo.undo, desc = "Show undo history", }
+  end
+  local telescope_project_available, _ = pcall(require, "project_nvim")
+  if telescope_project_available then
+    maps.n["<leader>fp"] = { require("telescope").extensions.projects.projects, desc = "Find projects", }
   end
 end
 
 --[ ToggleTerm
 if is_available("toggleterm.nvim") then
-  maps.n["<leader>th"] = {
-    "<CMD>ToggleTerm size=15 direction=horizontal<cr>",
-    desc = "ToggleTerm horizontal split",
-  }
+  maps.n["<leader>th"] = { "<CMD>ToggleTerm size=15 direction=horizontal<cr>", desc = "ToggleTerm horizontal split", }
   if vim.fn.executable("lnav") == 1 then
     maps.n["<leader>tL"] = {
       function()
@@ -158,35 +145,26 @@ end
 
 --]
 local function ui_notify(str)
-  if vim.g.ui_notifications_enabled then
-    notify(str)
-  end
+  if vim.g.ui_notifications_enabled then notify(str) end
 end
+
 local function bool2str(bool)
   return bool and "on" or "off"
 end
+
 local function toggle_lazyreadraw()
   vim.opt.lazyredraw = not vim.opt.lazyredraw:get()
   ui_notify(("lazy redraw %s"):format(bool2str(vim.opt.lazyredraw:get())))
 end
 
-maps.n["<leader>ur"] = {
-  toggle_lazyreadraw,
-  desc = "Toggle lazyredraw",
-}
+maps.n["<leader>ur"] = { toggle_lazyreadraw, desc = "Toggle lazyredraw", }
 
 if is_available("nvim-treesitter-context") then
-  maps.n["[c"] = {
-    function()
-      require("treesitter-context").go_to_context()
-    end,
-    desc = "Go to context",
-  }
+  maps.n["[c"] = { function() require("treesitter-context").go_to_context() end, desc = "Go to context", }
 end
 
 if is_available("nvim-transparent") then
-  maps.n["<leader>uT"] =
-    { "<CMD>TransparentToggle<CR>", desc = "Toggle tranparency" }
+  maps.n["<leader>uT"] = { "<CMD>TransparentToggle<CR>", desc = "Toggle tranparency" }
 end
 
 -- stylua: ignore start
@@ -204,147 +182,81 @@ end
 -- stylua: ignore end
 
 if is_available("diffview.nvim") then
-  maps.n["<leader>gd"] = {
-    "<CMD>DiffviewOpen<CR>",
-    desc = "Open diff view (g? help)",
-    silent = true,
-  }
+  maps.n["<leader>gd"] = { "<CMD>DiffviewOpen<CR>", desc = "Open diff view (g? help)", silent = true, }
   maps.n["<leader>gC"] = { "<CMD>DiffviewClose<CR>", desc = "Close diff view" }
-  maps.n["<leader>gh"] = {
-    "<CMD>DiffviewFileHistory % -f<CR>",
-    desc = "Open file history",
-    silent = true,
-  }
-  maps.n["<leader>gH"] = {
-    "<CMD>DiffviewFileHistory<CR>",
-    desc = "Open branch history",
-    silent = true,
-  }
-  maps.x["<leader>gh"] =
-    { ":DiffviewFileHistory<CR>", desc = "Open line history", silent = true }
+  maps.n["<leader>gh"] = { "<CMD>DiffviewFileHistory % -f<CR>", desc = "Open file history", silent = true, }
+  maps.n["<leader>gH"] = { "<CMD>DiffviewFileHistory<CR>", desc = "Open branch history", silent = true, }
+  maps.x["<leader>gh"] = { ":DiffviewFileHistory<CR>", desc = "Open line history", silent = true }
 end
-
 -- GitSigns
 if is_available("gitsigns.nvim") then
-  maps.n["<leader>gR"] = {
-    function()
-      require("gitsigns").reset_buffer()
-    end,
-    desc = "Reset Git buffer",
-  }
-  maps.n["<leader>gr"] = {
-    function()
-      require("gitsigns").reset_hunk()
-    end,
-    desc = "Reset Git hunk",
-  }
-  maps.n["<leader>gD"] = {
-    require("gitsigns").toggle_deleted,
-    desc = "Toggle deleted lines",
-  }
+  maps.n["<leader>gR"] = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer", }
+  maps.n["<leader>gr"] = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk", }
+  maps.n["<leader>gD"] = { require("gitsigns").toggle_deleted, desc = "Toggle deleted lines", }
 end
 
 if is_available("code_runner.nvim") then
   local icon
-  if vim.g.icons_enabled then
-    icon = "󰐍 "
-  else
-    icon = ""
-  end
+  if vim.g.icons_enabled then icon = "󰐍 " else icon = "" end
   maps.n["<leader>r"] = { desc = icon .. "Code runner" }
   maps.n["<leader>rr"] = { "<CMD>RunCode<CR>", desc = "Run code" }
   maps.n["<leader>rf"] = { "<CMD>RunFile<CR>", desc = "Run file" }
   maps.n["<leader>rt"] = { "<CMD>RunFile tab<CR>", desc = "Run file tab" }
   maps.n["<leader>rc"] = { "<CMD>RunClose<CR>", desc = "Close runner" }
-  maps.n["<leader>rp"] =
-    { "<CMD>RunFile toggleterm<CR>", desc = "Run file pop up (toggleterm)" }
+  maps.n["<leader>rp"] = { "<CMD>RunFile toggleterm<CR>", desc = "Run file pop up (toggleterm)" }
 end
 
 if is_available("sniprun") then
   -- for some reason there is error when using "<CR>" instead of ":"
   for _, mode in pairs { "n", "v" } do
-  maps[mode]["<leader>rs"] = { ":SnipRun<CR>", desc = "Run snippet" }
-  maps[mode]["<leader>rC"] = { ":SnipClose<CR>", desc = "Close SnipRun" }
+    maps[mode]["<leader>rs"] = { ":SnipRun<CR>", desc = "Run snippet" }
+    maps[mode]["<leader>rC"] = { ":SnipClose<CR>", desc = "Close SnipRun" }
   end
 end
 
 if is_available("neotest") then
   local icon
-  if vim.g.icons_enabled then
-    icon = " "
-  else
-    icon = ""
-  end
+  if vim.g.icons_enabled then icon = " " else icon = "" end
   maps.n["<leader>n"] = { false, desc = icon .. "Neotest" }
   maps.n["<leader>nr"] = { "<CMD>NeotestRun<CR>", desc = "Run nearest test" }
-  maps.n["<leader>nf"] =
-    { "<CMD>NeotestRunFile<CR>", desc = "Run tests in current file" }
+  maps.n["<leader>nf"] = { "<CMD>NeotestRunFile<CR>", desc = "Run tests in current file" }
   maps.n["<leader>nS"] = { "<CMD>NeotestStop<CR>", desc = "Stop running test" }
-  maps.n["<leader>nd"] =
-    { "<CMD>NeotestRunDap<CR>", desc = "Run test in debugger" }
-  maps.n["<leader>na"] =
-    { "<CMD>NeotestAttach<CR>", desc = "Attach to running test" }
-  maps.n["<leader>ns"] =
-    { "<CMD>NeotestSummaryToggle<CR>", desc = "Toggle test summary window" }
-  maps.n["<leader>no"] =
-    { "<CMD>NeotestOutput<CR>", desc = "Show test output" }
-  maps.n["<leader>nO"] =
-    { "<CMD>NeotestOutputToggle<CR>", desc = "Toggle test output window" }
-  maps.n["<leader>nk"] = {
-    "<CMD>NeotestJumpPreviousFailed<CR>",
-    desc = "Jump to previous failed test",
-  }
-  maps.n["<leader>nj"] =
-    { "<CMD>NeotestJumpNextFailed<CR>", desc = "Jump to next failed test" }
+  maps.n["<leader>nd"] = { "<CMD>NeotestRunDap<CR>", desc = "Run test in debugger" }
+  maps.n["<leader>na"] = { "<CMD>NeotestAttach<CR>", desc = "Attach to running test" }
+  maps.n["<leader>ns"] = { "<CMD>NeotestSummaryToggle<CR>", desc = "Toggle test summary window" }
+  maps.n["<leader>no"] = { "<CMD>NeotestOutput<CR>", desc = "Show test output" }
+  maps.n["<leader>nO"] = { "<CMD>NeotestOutputToggle<CR>", desc = "Toggle test output window" }
+  maps.n["<leader>nk"] = { "<CMD>NeotestJumpPreviousFailed<CR>", desc = "Jump to previous failed test", }
+  maps.n["<leader>nj"] = { "<CMD>NeotestJumpNextFailed<CR>", desc = "Jump to next failed test" }
 end
 
-maps.n["<leader>rn"] =
-  { "<CMD>BetterLuafile<CR>", desc = "Run lua file with nvim-lua" }
+maps.n["<leader>rn"] = { "<CMD>BetterLuafile<CR>", desc = "Run lua file with nvim-lua" }
 
 if is_available("trouble.nvim") then
   local icon
-  if vim.g.icons_enabled then
-    icon = "󱠪 "
-  else
-    icon = ""
-  end
+  if vim.g.icons_enabled then icon = "󱠪 " else icon = "" end
   maps.n["<leader>x"] = { desc = icon .. "Trouble" }
-  maps.n["<leader>xr"] =
-    { "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)" }
-  maps.n["<leader>xf"] =
-    { "<CMD>Trouble lsp_definitions<CR>", desc = "Definitions (Trouble)" }
-  maps.n["<leader>xq"] =
-    { "<CMD>Trouble quickfix<CR>", desc = "QuickFix (Trouble)" }
-  maps.n["<leader>xl"] =
-    { "<CMD>Trouble loclist<CR>", desc = "LocationList (Trouble)" }
+  maps.n["<leader>xr"] = { "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)" }
+  maps.n["<leader>xf"] = { "<CMD>Trouble lsp_definitions<CR>", desc = "Definitions (Trouble)" }
+  maps.n["<leader>xq"] = { "<CMD>Trouble quickfix<CR>", desc = "QuickFix (Trouble)" }
+  maps.n["<leader>xl"] = { "<CMD>Trouble loclist<CR>", desc = "LocationList (Trouble)" }
   maps.n["<leader>xt"] = { "<CMD>TodoTrouble<CR>", desc = "TODO list" }
 end
 
 if is_available("glow.nvim") then
   local icon
-  if vim.g.icons_enabled then
-    icon = "󰽛 "
-  else
-    icon = ""
-  end
+  if vim.g.icons_enabled then icon = "󰽛 " else icon = "" end
   maps.n["<leader>m"] = { desc = icon .. "Markdown" }
   maps.n["<leader>mM"] = { "<CMD>Glow<CR>", desc = "Markdown Glow" }
 end
 
 if is_available("markdown-preview.nvim") then
   local icon
-  if vim.g.icons_enabled then
-    icon = "󰽛 "
-  else
-    icon = ""
-  end
+  if vim.g.icons_enabled then icon = "󰽛 " else icon = "" end
   maps.n["<leader>m"] = { desc = icon .. "Markdown" }
-  maps.n["<leader>mm"] =
-    { "<CMD>MarkdownPreview<CR>", desc = "MarkdownPreview" }
-  maps.n["<leader>mt"] =
-    { "<CMD>MarkdownPreviewToggle<CR>", desc = "MarkdownPreview Toggle" }
-  maps.n["<leader>ms"] =
-    { "<CMD>MarkdownPreviewStop<CR>", desc = "MarkdownPreview Stop" }
+  maps.n["<leader>mm"] = { "<CMD>MarkdownPreview<CR>", desc = "MarkdownPreview" }
+  maps.n["<leader>mt"] = { "<CMD>MarkdownPreviewToggle<CR>", desc = "MarkdownPreview Toggle" }
+  maps.n["<leader>ms"] = { "<CMD>MarkdownPreviewStop<CR>", desc = "MarkdownPreview Stop" }
 end
 
 -- Move Lines
@@ -358,55 +270,19 @@ maps.t["<esc><esc>"] = { "<c-\\><c-n>", desc = "Enter Normal Mode" }
 -- Smart Splits (remapped on Meta key)
 if is_available("smart-splits.nvim") then
   -- Resize with arrows
-  maps.n["<M-Up>"] = {
-    function()
-      require("smart-splits").resize_up()
-    end,
-    desc = "Resize split up",
-  }
-  maps.n["<M-Down>"] = {
-    function()
-      require("smart-splits").resize_down()
-    end,
-    desc = "Resize split down",
-  }
-  maps.n["<M-Left>"] = {
-    function()
-      require("smart-splits").resize_left()
-    end,
-    desc = "Resize split left",
-  }
-  maps.n["<M-Right>"] = {
-    function()
-      require("smart-splits").resize_right()
-    end,
-    desc = "Resize split right",
-  }
+  maps.n["<M-Up>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up", }
+  maps.n["<M-Down>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down", }
+  maps.n["<M-Left>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left", }
+  maps.n["<M-Right>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right", }
 else
   maps.n["<M-Up>"] = { "<CMD>resize -2<CR>", desc = "Resize split up" }
   maps.n["<M-Down>"] = { "<CMD>resize +2<CR>", desc = "Resize split down" }
-  maps.n["<M-Left>"] =
-    { "<CMD>vertical resize -2<CR>", desc = "Resize split left" }
-  maps.n["<M-Right>"] =
-    { "<CMD>vertical resize +2<CR>", desc = "Resize split right" }
+  maps.n["<M-Left>"] = { "<CMD>vertical resize -2<CR>", desc = "Resize split left" }
+  maps.n["<M-Right>"] = { "<CMD>vertical resize +2<CR>", desc = "Resize split right" }
 end
 
 -- add more text objects for "in" and "around"
-for _, char in ipairs {
-  "_",
-  ".",
-  ":",
-  ",",
-  ";",
-  "|",
-  "/",
-  "\\",
-  "*",
-  "+",
-  "%",
-  "`",
-  "?",
-} do
+for _, char in ipairs { "_", ".", ":", ",", ";", "|", "/", "\\", "*", "+", "%", "`", "?", } do
   for _, mode in ipairs { "x", "o" } do
     maps[mode]["i" .. char] = {
       string.format(":<C-u>silent! normal! f%sF%slvt%s<CR>", char, char, char),
