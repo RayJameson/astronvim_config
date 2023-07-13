054c36247bb782f30fbeb29d009e9991d4f1a3a3
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-13 10:32:12 +0600

    refactor(mappings): use `prefix` variable and concatenation

--- mappings.lua
+++ mappings.lua
@@ -115,23 +115,25 @@ if is_available("mini.ai") then
 end
 
 if is_available("telescope.nvim") then
-  maps.n["<leader>fg"] = { require("telescope.builtin").git_files, desc = "Find git files" }
-  maps.n["<leader>fs"] = { require("telescope.builtin").spell_suggest, desc = "Show spell suggestions" }
+  local prefix = "<leader>f"
+  maps.n[prefix .. "g"] = { require("telescope.builtin").git_files, desc = "Find git files" }
+  maps.n[prefix .. "s"] = { require("telescope.builtin").spell_suggest, desc = "Show spell suggestions" }
   local telescope_undo_available, _ = pcall(require, "telescope-undo")
   if telescope_undo_available then
-    maps.n["<leader>fu"] = { require("telescope").extensions.undo.undo, desc = "Show undo history" }
+    maps.n[prefix.. "u"] = { require("telescope").extensions.undo.undo, desc = "Show undo history" }
   end
   local telescope_project_available, _ = pcall(require, "project_nvim")
   if telescope_project_available then
-    maps.n["<leader>fp"] = { require("telescope").extensions.projects.projects, desc = "Find projects" }
+    maps.n[prefix .. "p"] = { require("telescope").extensions.projects.projects, desc = "Find projects" }
   end
 end
 
 --[ ToggleTerm
 if is_available("toggleterm.nvim") then
-  maps.n["<leader>th"] = { "<CMD>ToggleTerm size=15 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" }
+  local prefix = "<leader>t"
+  maps.n[prefix .. "h"] = { "<CMD>ToggleTerm size=15 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" }
   if vim.fn.executable("lnav") == 1 then
-    maps.n["<leader>tL"] = {
+    maps.n[prefix .. "L"] = {
       function() toggle_term_cmd("lnav " .. vim.fn.expand("%:p")) end,
       silent = true,
       desc = "ToggleTerm lnav",
@@ -163,89 +165,102 @@ end
 
 -- stylua: ignore start
 if is_available("nvim-dap") then
+  local prefix = "<leader>d"
   maps.n["<F8>"] = { function() require("dap").toggle_breakpoint() end, desc = "Debugger: Toggle Breakpoint", }
   maps.n["<F9>"] = { function() require("dap").step_over() end, desc = "Debugger: Step Over", }
   maps.n["<F10>"] = { function() require("dap").step_into() end, desc = "Debugger: Step Into", }
   maps.n["<F22>"] = { function() require("dap").step_out() end, desc = "Debugger: Step Out", } -- Shift+F10
-  maps.n["<leader>db"] = { function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint (F8)", }
-  maps.n["<leader>di"] = { function() require("dap").step_into() end, desc = "Step Into (F10)", }
-  maps.n["<leader>dO"] = { function() require("dap").step_out() end, desc = "Step Out (S-F10)", }
-  maps.n["<leader>do"] = { function() require("dap").step_over() end, desc = "Step Over (F9)", }
+  maps.n[prefix .. "b"] = { function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint (F8)", }
+  maps.n[prefix .. "i"] = { function() require("dap").step_into() end, desc = "Step Into (F10)", }
+  maps.n[prefix .. "O"] = { function() require("dap").step_out() end, desc = "Step Out (S-F10)", }
+  maps.n[prefix .. "o"] = { function() require("dap").step_over() end, desc = "Step Over (F9)", }
   maps.n["<F11>"] = false
 end
 -- stylua: ignore end
 
 if is_available("diffview.nvim") then
-  maps.n["<leader>gd"] = { "<CMD>DiffviewOpen<CR>", desc = "Open diff view (g? help)", silent = true }
-  maps.n["<leader>gC"] = { "<CMD>DiffviewClose<CR>", desc = "Close diff view" }
-  maps.n["<leader>gh"] = { "<CMD>DiffviewFileHistory % -f<CR>", desc = "Open file history", silent = true }
-  maps.n["<leader>gH"] = { "<CMD>DiffviewFileHistory<CR>", desc = "Open branch history", silent = true }
-  maps.x["<leader>gh"] = { ":DiffviewFileHistory<CR>", desc = "Open line history", silent = true }
+  local prefix = "<leader>g"
+  maps.n[prefix .. "d"] = { "<CMD>DiffviewOpen<CR>", desc = "Open diff view (g? help)", silent = true }
+  maps.n[prefix .. "C"] = { "<CMD>DiffviewClose<CR>", desc = "Close diff view" }
+  maps.n[prefix .. "h"] = { "<CMD>DiffviewFileHistory % -f<CR>", desc = "Open file history", silent = true }
+  maps.n[prefix .. "H"] = { "<CMD>DiffviewFileHistory<CR>", desc = "Open branch history", silent = true }
+  maps.x[prefix .. "h"] = { ":DiffviewFileHistory<CR>", desc = "Open line history", silent = true }
 end
 -- GitSigns
 if is_available("gitsigns.nvim") then
-  maps.n["<leader>gR"] = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" }
-  maps.n["<leader>gr"] = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" }
-  maps.n["<leader>gD"] = { require("gitsigns").toggle_deleted, desc = "Toggle deleted lines" }
+  local prefix = "<leader>g"
+  maps.n[prefix .. "R"] = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" }
+  maps.n[prefix .. "r"] = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" }
+  maps.n[prefix .. "D"] = { require("gitsigns").toggle_deleted, desc = "Toggle deleted lines" }
 end
 
 if is_available("code_runner.nvim") then
   local icon = vim.g.icons_enabled and "󰐍 " or ""
-  maps.n["<leader>r"] = { desc = icon .. "Code runner" }
-  maps.n["<leader>rr"] = { "<CMD>RunCode<CR>", desc = "Run code" }
-  maps.n["<leader>rf"] = { "<CMD>RunFile<CR>", desc = "Run file" }
-  maps.n["<leader>rt"] = { "<CMD>RunFile tab<CR>", desc = "Run file tab" }
-  maps.n["<leader>rc"] = { "<CMD>RunClose<CR>", desc = "Close runner" }
-  maps.n["<leader>rp"] = { "<CMD>RunFile toggleterm<CR>", desc = "Run file pop up (toggleterm)" }
+  local prefix = "<leader>r"
+  maps.n[prefix] = { desc = icon .. "Code runner" }
+  maps.n[prefix .. "r"] = { "<CMD>RunCode<CR>", desc = "Run code" }
+  maps.n[prefix .. "f"] = { "<CMD>RunFile<CR>", desc = "Run file" }
+  maps.n[prefix .. "t"] = { "<CMD>RunFile tab<CR>", desc = "Run file tab" }
+  maps.n[prefix .. "c"] = { "<CMD>RunClose<CR>", desc = "Close runner" }
+  maps.n[prefix .. "p"] = { "<CMD>RunFile toggleterm<CR>", desc = "Run file pop up (toggleterm)" }
 end
 
 if is_available("sniprun") then
   -- for some reason there is error when using "<CR>" instead of ":"
   for _, mode in pairs { "n", "v" } do
-    maps[mode]["<leader>rs"] = { ":SnipRun<CR>", desc = "Run snippet" }
-    maps[mode]["<leader>rC"] = { ":SnipClose<CR>", desc = "Close SnipRun" }
+    local icon = vim.g.icons_enabled and " " or ""
+    local prefix = "<leader>r"
+    if maps[mode][prefix] == nil or maps[mode][prefix].desc == "prefix" then
+      maps[mode][prefix] = { desc = icon .. "Snip run" }
+    end
+    maps[mode][prefix .. "s"] = { ":SnipRun<CR>", desc = "Run snippet" }
+    maps[mode][prefix .. "C"] = { ":SnipClose<CR>", desc = "Close SnipRun" }
   end
 end
 
 if is_available("neotest") then
   local icon = vim.g.icons_enabled and " " or ""
-  maps.n["<leader>n"] = { false, desc = icon .. "Neotest" }
-  maps.n["<leader>nr"] = { "<CMD>NeotestRun<CR>", desc = "Run nearest test" }
-  maps.n["<leader>nf"] = { "<CMD>NeotestRunFile<CR>", desc = "Run tests in current file" }
-  maps.n["<leader>nS"] = { "<CMD>NeotestStop<CR>", desc = "Stop running test" }
-  maps.n["<leader>nd"] = { "<CMD>NeotestRunDap<CR>", desc = "Run test in debugger" }
-  maps.n["<leader>na"] = { "<CMD>NeotestAttach<CR>", desc = "Attach to running test" }
-  maps.n["<leader>ns"] = { "<CMD>NeotestSummaryToggle<CR>", desc = "Toggle test summary window" }
-  maps.n["<leader>no"] = { "<CMD>NeotestOutput<CR>", desc = "Show test output" }
-  maps.n["<leader>nO"] = { "<CMD>NeotestOutputToggle<CR>", desc = "Toggle test output window" }
-  maps.n["<leader>nk"] = { "<CMD>NeotestJumpPreviousFailed<CR>", desc = "Jump to previous failed test" }
-  maps.n["<leader>nj"] = { "<CMD>NeotestJumpNextFailed<CR>", desc = "Jump to next failed test" }
+  local prefix = "<leader>n"
+  maps.n[prefix .. ""] = { desc = icon .. "Neotest" }
+  maps.n[prefix .. "r"] = { "<CMD>NeotestRun<CR>", desc = "Run nearest test" }
+  maps.n[prefix .. "f"] = { "<CMD>NeotestRunFile<CR>", desc = "Run tests in current file" }
+  maps.n[prefix .. "S"] = { "<CMD>NeotestStop<CR>", desc = "Stop running test" }
+  maps.n[prefix .. "d"] = { "<CMD>NeotestRunDap<CR>", desc = "Run test in debugger" }
+  maps.n[prefix .. "a"] = { "<CMD>NeotestAttach<CR>", desc = "Attach to running test" }
+  maps.n[prefix .. "s"] = { "<CMD>NeotestSummaryToggle<CR>", desc = "Toggle test summary window" }
+  maps.n[prefix .. "o"] = { "<CMD>NeotestOutput<CR>", desc = "Show test output" }
+  maps.n[prefix .. "O"] = { "<CMD>NeotestOutputToggle<CR>", desc = "Toggle test output window" }
+  maps.n[prefix .. "k"] = { "<CMD>NeotestJumpPreviousFailed<CR>", desc = "Jump to previous failed test" }
+  maps.n[prefix .. "j"] = { "<CMD>NeotestJumpNextFailed<CR>", desc = "Jump to next failed test" }
 end
 
 maps.n["<leader>rn"] = { "<CMD>BetterLuafile<CR>", desc = "Run lua file with nvim-lua" }
 
 if is_available("trouble.nvim") then
   local icon = vim.g.icons_enabled and "󱠪 " or ""
-  maps.n["<leader>x"] = { desc = icon .. "Trouble" }
-  maps.n["<leader>xr"] = { "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)" }
-  maps.n["<leader>xf"] = { "<CMD>Trouble lsp_definitions<CR>", desc = "Definitions (Trouble)" }
-  maps.n["<leader>xq"] = { "<CMD>Trouble quickfix<CR>", desc = "QuickFix (Trouble)" }
-  maps.n["<leader>xl"] = { "<CMD>Trouble loclist<CR>", desc = "LocationList (Trouble)" }
-  maps.n["<leader>xt"] = { "<CMD>TodoTrouble<CR>", desc = "TODO list" }
+  local prefix = "<leader>x"
+  maps.n[prefix] = { desc = icon .. "Trouble" }
+  maps.n[prefix .. "r"] = { "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)" }
+  maps.n[prefix .. "f"] = { "<CMD>Trouble lsp_definitions<CR>", desc = "Definitions (Trouble)" }
+  maps.n[prefix .. "q"] = { "<CMD>Trouble quickfix<CR>", desc = "QuickFix (Trouble)" }
+  maps.n[prefix .. "l"] = { "<CMD>Trouble loclist<CR>", desc = "LocationList (Trouble)" }
+  maps.n[prefix .. "t"] = { "<CMD>TodoTrouble<CR>", desc = "TODO list" }
 end
 
 if is_available("glow.nvim") then
   local icon = vim.g.icons_enabled and "󰽛 " or ""
-  maps.n["<leader>m"] = { desc = icon .. "Markdown" }
-  maps.n["<leader>mM"] = { "<CMD>Glow<CR>", desc = "Markdown Glow" }
+  local prefix = "<leader>m"
+  maps.n[prefix] = { desc = icon .. "Markdown" }
+  maps.n[prefix .. "M"] = { "<CMD>Glow<CR>", desc = "Markdown Glow" }
 end
 
 if is_available("markdown-preview.nvim") then
   local icon = vim.g.icons_enabled and "󰽛 " or ""
-  maps.n["<leader>m"] = { desc = icon .. "Markdown" }
-  maps.n["<leader>mm"] = { "<CMD>MarkdownPreview<CR>", desc = "MarkdownPreview" }
-  maps.n["<leader>mt"] = { "<CMD>MarkdownPreviewToggle<CR>", desc = "MarkdownPreview Toggle" }
-  maps.n["<leader>ms"] = { "<CMD>MarkdownPreviewStop<CR>", desc = "MarkdownPreview Stop" }
+  local prefix = "<leader>m"
+  maps.n[prefix] = { desc = icon .. "Markdown" }
+  maps.n[prefix .. "m"] = { "<CMD>MarkdownPreview<CR>", desc = "MarkdownPreview" }
+  maps.n[prefix .. "t"] = { "<CMD>MarkdownPreviewToggle<CR>", desc = "MarkdownPreview Toggle" }
+  maps.n[prefix .. "s"] = { "<CMD>MarkdownPreviewStop<CR>", desc = "MarkdownPreview Stop" }
 end
 
 -- Move Lines
