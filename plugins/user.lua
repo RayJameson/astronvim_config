6b5dcecbf92c0a373b0914f550a2f6b736242d67
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-14 19:59:21 +0600

    refactor(hop): replace with my fork, remove case sensetivity
    origin is abandoned, so I merged fix for case_sensetivity option
    so it does work even if `vim.o.smartcase` is enabled

--- plugins/user.lua
+++ plugins/user.lua
@@ -397,8 +397,10 @@ return {
     event = "User AstroFile",
   },
   {
-    "phaazon/hop.nvim",
-    opts = {},
+    "RayJameson/hop.nvim",
+    opts = {
+      case_insensitive = false,
+    },
     keys = function()
       local hop = require("hop")
       local directions = require("hop.hint").HintDirection
@@ -407,25 +409,21 @@ return {
           "f",
           function() hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true } end,
           desc = "Move to the next char",
-          { silent = true },
         },
         {
           "F",
           function() hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true } end,
           desc = "Move to the previous char",
-          { silent = true },
         },
         {
           "t",
           function() hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 } end,
           desc = "Move before next char",
-          { silent = true },
         },
         {
           "T",
           function() hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 } end,
           desc = "Move after previous char",
-          { silent = true },
         },
       }
     end,
