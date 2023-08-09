76b28eea692d3e79b7a041d2448798f89ccd92d3
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-08-09 20:15:51 +0600

    refactor(autocmds): move `leave_snippet` func body to callback body

--- autocmds.lua
+++ autocmds.lua
@@ -41,21 +41,20 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = "*",
   callback = function() vim.api.nvim_win_set_option(0, "foldcolumn", "auto:9") end,
 })
-local function leave_snippet()
-  if
-    ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-    and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-    and not require("luasnip").session.jump_active
-  then
-    require("luasnip").unlink_current()
-  end
-end
 
 -- stop snippets when you leave to normal mode
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = "*",
   group = vim.api.nvim_create_augroup("ModeChangedGroup", { clear = true }),
-  callback = function() leave_snippet() end,
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
 })
 
 -- remove colorcolumn for qf
@@ -74,9 +73,7 @@ vim.api.nvim_create_augroup("DynamicColorColumn", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   group = "DynamicColorColumn",
-  callback = function()
-    vim.wo.colorcolumn = "120"
-  end,
+  callback = function() vim.wo.colorcolumn = "120" end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
