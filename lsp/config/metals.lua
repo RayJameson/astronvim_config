f74c7482b8e7cb9940a173f8f2cee897ce364fc3
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-08-17 21:02:45 +0600

    fix(metals): add cache path to PATH, also add default settings to lsp

---
+++ lsp/config/metals.lua
@@ -0,0 +1,6 @@
+return {
+  settings = {
+    showImplicitArguments = true,
+    excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+  },
+}
--- plugins/metals.lua
+++ plugins/metals.lua
@@ -2,7 +2,7 @@ return {
   "scalameta/nvim-metals",
   dependencies = "nvim-lua/plenary.nvim",
   init = function()
-    vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/nvim-metals"
+    vim.env.PATH =  vim.fn.stdpath("cache") .. "/nvim-metals:" .. vim.env.PATH
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "scala", "sbt", "java" },
       callback = function() require("metals").initialize_or_attach(require("astronvim.utils.lsp").config("metals")) end,
