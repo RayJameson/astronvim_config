608f9390e173632d2713b79a5085cdf0682df245
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-08-20 18:48:10 +0600

    feat(plugins): add `openingh-nvim` plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -84,4 +84,16 @@ return {
       "DevdocsUpdateAll",
     },
   },
+  {
+    "almo7aya/openingh.nvim",
+    cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines" },
+    keys = function()
+      local prefix = "<leader>g"
+      return {
+        { prefix .. "o", "<cmd>OpenInGHRepo<CR>", desc = "Open git repo in web", mode = { "n" } },
+        { prefix .. "f", "<cmd>OpenInGHFile<CR>", desc = "Open git file in web", mode = { "n" } },
+        { prefix .. "f", "<cmd>OpenInGHFileLines<CR>", desc = "Open git lines in web", mode = { "x" } },
+      }
+    end,
+  },
 }
