5d31ebd924dc7d7099d080bebd58b4a3d81ecb77
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-08-22 01:20:30 +0600

    fix(metals): lazy load `metals` on ft, move `init` to `config`

--- plugins/metals.lua
+++ plugins/metals.lua
@@ -1,8 +1,9 @@
 return {
   "scalameta/nvim-metals",
   dependencies = "nvim-lua/plenary.nvim",
-  init = function()
-    vim.env.PATH =  vim.fn.stdpath("cache") .. "/nvim-metals:" .. vim.env.PATH
+  ft = { "scala", "sbt", "java" },
+  config = function() -- metals has no default config
+    vim.env.PATH = vim.fn.stdpath("cache") .. "/nvim-metals:" .. vim.env.PATH
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "scala", "sbt", "java" },
       callback = function() require("metals").initialize_or_attach(require("astronvim.utils.lsp").config("metals")) end,
