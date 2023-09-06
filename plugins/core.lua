f05cb9b1b067ac51df52398331d32deec807908c
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-09-06 22:12:35 +0600

    remove(scope): removed cyrillic mappings for escape
    "ол" is often used in words

--- plugins/core.lua
+++ plugins/core.lua
@@ -81,7 +81,7 @@ return {
     "max397574/better-escape.nvim",
     cond = not vim.g.vscode,
     opts = function(_, opts)
-      opts.mapping = { "JK", "JJ", "jk", "jj", "оо", "ол", "ОО", "ОЛ" }
+      opts.mapping = { "JK", "JJ", "jk", "jj" }
       opts.keys = function() return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>" end
     end,
   },
