c6f61f643f06a0ef794b1a4b5d48a9ce253e3382
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-09-19 12:26:37 +0600

    fix(devdocs): fix preview args

--- plugins/user.lua
+++ plugins/user.lua
@@ -101,7 +101,7 @@ return {
       previewer_cmd = vim.fn.executable("glow") == 1 and "glow" or nil,
       cmd_args = { "-s", "dark", "-w", "80" },
       picker_cmd = true,
-      picker_cmd_args = { "-p" },
+      picker_cmd_args = { "-s", "dark", "-w", "50" },
       float_win = { -- passed to nvim_open_win(), see :h api-floatwin
         relative = "editor",
         height = 35,
