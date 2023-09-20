21ab6fce0ade71e71e1562b8d967045c94e8a777
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-09-20 18:04:37 +0600

    feat(telescope): open one/multiple files in telescope on `<CR>`
    credit to AXGKl and jesseleite:
    https://github.com/AstroNvim/AstroNvim/issues/897#issuecomment-1407200375
    https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -51,6 +51,18 @@ return {
       actions.smart_send_to_qflist(prompt_bufnr)
       trouble.open("quickfix")
     end
+    local select_one_or_multi = function(prompt_bufnr)
+      local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+      local multi = picker:get_multi_selection()
+      if not vim.tbl_isempty(multi) then
+        require("telescope.actions").close(prompt_bufnr)
+        for _, j in pairs(multi) do
+          if j.path ~= nil then vim.cmd(string.format("%s %s", "edit", j.path)) end
+        end
+      else
+        require("telescope.actions").select_default(prompt_bufnr)
+      end
+    end
 
     for _, mode in ipairs { "i", "n" } do
       opts.defaults.mappings[mode] = extend_tbl(opts.defaults.mappings[mode], {
@@ -60,6 +72,7 @@ return {
         ["<M-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.smart_add_to_loclist + actions.open_loclist,
         ["<M-l>"] = actions.smart_send_to_loclist + actions.open_loclist,
+        ["<CR>"] = select_one_or_multi,
       })
     end
   end,
