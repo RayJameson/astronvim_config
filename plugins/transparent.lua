f3133b560619742dbdd4aef936e2f93c55aa09eb
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-11-22 16:51:09 +0300

    fix(transparent): move to user plugins, fix highlights changed upstrm

---
+++ plugins/transparent.lua
@@ -0,0 +1,53 @@
+local prefix = "<leader>u"
+return {
+  "xiyaowong/transparent.nvim",
+  lazy = false,
+  opts = {
+    -- table: default groups
+    groups = {
+      "Normal",
+      "NormalNC",
+      "Comment",
+      "Constant",
+      "Special",
+      "Identifier",
+      "Statement",
+      "PreProc",
+      "Type",
+      "Underlined",
+      "Todo",
+      "String",
+      "Function",
+      "Conditional",
+      "Repeat",
+      "Operator",
+      "Structure",
+      "LineNr",
+      "NonText",
+      "SignColumn",
+      "CursorLineNr",
+      "CursorLine",
+      "EndOfBuffer",
+    },
+    -- table: additional groups that should be cleared
+    extra_groups = {
+      "NormalFloat",
+      "NvimTreeNormal",
+      "NeoTreeNormal",
+      "NeoTreeFloatBorder",
+      "NeoTreeNormalNC",
+      "TelescopeNormal",
+      "TelescopePromptNormal",
+      "TelescopePreviewNormal",
+      "TelescopeResultsNormal",
+      "WinBar",
+      "WinBarNC",
+      "FloatBorder",
+    },
+    -- table: groups you don't want to clear
+    exclude_groups = {},
+  },
+  keys = {
+    { prefix .. "T", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
+  },
+}
