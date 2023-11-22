37a79e2924ada7b6b88701e653c22edb919f9d53
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-11-22 16:51:27 +0300

    feat(ruff_lsp): use common rules

--- lsp/config/ruff_lsp.lua
+++ lsp/config/ruff_lsp.lua
@@ -24,10 +24,12 @@ return {
   end,
   init_options = {
     settings = {
+      --"--ignore=D,PT009,ANN002,ANN003,ANN101,ANN102,ANN401,A003,RUF001,RUF002,RUF003,RET504,PD008,TD001,TD002,TD003,S101",
       args = {
-        "--select=ALL",
-        "--ignore=D,PT009,ANN002,ANN003,ANN101,ANN102,ANN401,A003,RUF001,RUF002,RUF003,RET504,PD008,TD001,TD002,TD003,S101",
-        "--line-length=79",
+        "--select=B,E,F,I,ANN,ASYNC,S,COM,C,ISC,PIE,PT,Q,TID,ARG,PLE,PLR,PLW,RUF",
+        "--ignore=ANN101,RUF100,PLR0913,ANN102,ANN401,PLR2004,S101",
+        "--line-length=120",
+        "fixable=A,B,C,D,E,F,G,I,N,Q,S,T,W,ANN,ARG,BLE,COM,DJ,DTZ,EM,ERA,EXE,FBT,ICN,INP,ISC,NPY,PD,PGH,PIE,PL,PT,PTH,PYI,RET,RSE,RUF,SIM,SLF,TCH,TID,TRY,UP,YTT",
       },
     },
   },
