2063ffe8f372d7ebaa65c121278729084a1d8ada
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-12-03 19:28:30 +0300

    refactor(highlights): early return if launched in vscode

--- highlights/astrotheme.lua
+++ highlights/astrotheme.lua
@@ -1,33 +1,33 @@
+if vim.g.vscode then return {} end
 local is_available = require("astronvim.utils").is_available
 return function(hl) -- or a function that returns a new table of colors to set
   local C = require("user.highlights.colors")
-  if not vim.g.vscode then
-    hl.CursorLineNr = { fg = C.blue_1 }
-    hl.LineNrAbove = { fg = C.pale_red }
-    hl.LineNrBelow = { fg = C.pale_green }
-    hl.DiagnosticVirtualTextHint = { fg = C.white, bg = "none" }
-    hl.DiagnosticVirtualTextWarn = { fg = C.equator, bg = "none" }
-    hl.DiagnosticVirtualTextInfo = { fg = C.wild_willow, bg = "none" }
-    hl.QuickFixLine = { fg = C.white }
-    hl.NeoTreeTabActive = { fg = C.fg, bold = true, italic = true }
-    hl.Keyword = { fg = C.purple, italic = true }
-    hl.Define = { fg = C.purple, italic = true }
-    hl.Include = { fg = C.purple, italic = true }
-    hl.Statement = { fg = C.purple, bg = C.none, italic = true } -- any statement
-    hl.Conditional = { fg = C.purple, bg = C.none, italic = true } -- if, then, else, endif, switch, etc.
-    hl.Repeat = { fg = C.purple, bg = C.none, italic = true } -- for, do, while, etc.
-    hl.Label = { fg = C.blue, bg = C.none, italic = true } -- case, default, etc.
-    hl.Exception = { fg = C.purple, bg = C.none, italic = true } -- try, catch, throw
-    hl.Boolean = { fg = C.bright_turquoise, bg = C.none } -- a boolean constant: TRUE, false
-    hl.DiffDelete = { fg = C.red }
-    hl["@lsp.typemod.function.global"] = { fg = C.blue_1 }
-    hl["@function.builtin"] = { fg = C.blue_1 }
-    hl["@variable.builtin"] = { fg = C.red_3 }
-    if is_available("mini.clue") then
-      hl.MiniClueDescGroup = { fg = C.purple }
-      hl.MiniClueDescSingle = { fg = C.blue }
-      hl.MiniClueNextKey = { fg = C.blue }
-    end
+  hl.CursorLineNr = { fg = C.blue_1 }
+  hl.LineNrAbove = { fg = C.pale_red }
+  hl.LineNrBelow = { fg = C.pale_green }
+  hl.DiagnosticVirtualTextHint = { fg = C.white, bg = "none" }
+  hl.DiagnosticVirtualTextWarn = { fg = C.equator, bg = "none" }
+  hl.DiagnosticVirtualTextInfo = { fg = C.wild_willow, bg = "none" }
+  hl.QuickFixLine = { fg = C.white }
+  hl.NeoTreeTabActive = { fg = C.fg, bold = true, italic = true }
+  hl.Keyword = { fg = C.purple, italic = true }
+  hl.Define = { fg = C.purple, italic = true }
+  hl.Include = { fg = C.purple, italic = true }
+  hl.Statement = { fg = C.purple, bg = C.none, italic = true } -- any statement
+  hl.Conditional = { fg = C.purple, bg = C.none, italic = true } -- if, then, else, endif, switch, etc.
+  hl.Repeat = { fg = C.purple, bg = C.none, italic = true } -- for, do, while, etc.
+  hl.Label = { fg = C.blue, bg = C.none, italic = true } -- case, default, etc.
+  hl.Exception = { fg = C.purple, bg = C.none, italic = true } -- try, catch, throw
+  hl.Boolean = { fg = C.bright_turquoise, bg = C.none } -- a boolean constant: TRUE, false
+  hl.DiffDelete = { fg = C.red }
+  hl["@lsp.typemod.function.global"] = { fg = C.blue_1 }
+  hl["@function.builtin"] = { fg = C.blue_1 }
+  hl["@variable.builtin"] = { fg = C.red_3 }
+  if is_available("mini.clue") then
+    hl.MiniClueDescGroup = { fg = C.purple }
+    hl.MiniClueDescSingle = { fg = C.blue }
+    hl.MiniClueNextKey = { fg = C.blue }
+    hl.MiniClueSeparator = { fg = C.grey_1 }
   end
   if vim.g.transparent_enabled then
     hl.TreesitterContextLineNumber = { fg = C.pale_red }
