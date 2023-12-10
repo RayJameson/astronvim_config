cf0986fed4d8b2bbd40897fb0f22bc6cfff06ab5
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-12-10 11:43:52 +0300

    feat(oil): always show file preview

--- plugins/oil.lua
+++ plugins/oil.lua
@@ -31,6 +31,15 @@ return {
         end,
       })
     end
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilEnter",
+      callback = vim.schedule_wrap(function(args)
+        local oil = require("oil")
+        if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+          oil.select({ preview = true })
+        end
+      end),
+    })
   end,
   opts = {
     skip_confirm_for_simple_edits = true,
