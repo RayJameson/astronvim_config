47ec3b2a378344112cf92455e809a8b47a7104f4
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-12-16 08:34:19 +0300

    fix(mini-clue): remove custom logic, doesn't update on register change

--- plugins/mini-clue.lua
+++ plugins/mini-clue.lua
@@ -15,74 +15,6 @@ return {
           end
         end
       end
-      local describe_registers = function(mode, prefix)
-        local make_register_content = function(register)
-          local ok, value = pcall(vim.fn.getreg, register, 1)
-          if not ok or value == "" then return nil end
-          return vim.inspect(value)
-        end
-
-        local all_registers = vim.split('0123456789abcdefghijklmnopqrstuvwxyz*+"-:.%/#', "")
-        local register_map = {
-          ["0"] = "Latest yank",
-          ["1"] = "Latest big delete",
-          ['"'] = "Default register",
-          ["#"] = "Alternate buffer",
-          ["%"] = "Name of the current file",
-          ["*"] = "Selection clipboard",
-          ["+"] = "System clipboard",
-          ["-"] = "Latest small delete",
-          ["."] = "Latest inserted text",
-          ["/"] = "Latest search pattern",
-          [":"] = "Latest executed command",
-          ["="] = "Result of expression",
-          ["_"] = "Black hole",
-        }
-
-        local res = {}
-        for _, register in ipairs(all_registers) do
-          local register_content = make_register_content(register)
-          if register_content then
-            local register_desc = register_map[register] and register_map[register] .. ": " or ""
-            table.insert(res, {
-              mode = mode,
-              keys = prefix .. register,
-              desc = register_desc .. register_content,
-            })
-          end
-        end
-        table.insert(res, { mode = mode, keys = prefix .. "=", desc = "Result of expression" })
-
-        return res
-      end
-      local registers_with_content = {
-        -- Normal mode
-        describe_registers("n", '"'),
-
-        -- Visual mode
-        describe_registers("x", '"'),
-
-        -- Insert mode
-        describe_registers("i", "<C-r>"),
-
-        { mode = "i", keys = "<C-r><C-r>", desc = "+Insert literally" },
-        describe_registers("i", "<C-r><C-r>"),
-
-        { mode = "i", keys = "<C-r><C-o>", desc = "+Insert literally + not auto-indent" },
-        describe_registers("i", "<C-r><C-o>"),
-
-        { mode = "i", keys = "<C-r><C-p>", desc = "+Insert + fix indent" },
-        describe_registers("i", "<C-r><C-p>"),
-
-        -- Command-line mode
-        describe_registers("c", "<C-r>"),
-
-        { mode = "c", keys = "<C-r><C-r>", desc = "+Insert literally" },
-        describe_registers("c", "<C-r><C-r>"),
-
-        { mode = "c", keys = "<C-r><C-o>", desc = "+Insert literally" },
-        describe_registers("c", "<C-r><C-o>"),
-      }
 
       opts.triggers = {
         -- Built-in completion
@@ -113,7 +45,7 @@ return {
       }
       opts.clues = {
         astronvim_clues,
-        registers_with_content,
+        miniclue.gen_clues.registers(),
         miniclue.gen_clues.builtin_completion(),
         miniclue.gen_clues.g(),
         miniclue.gen_clues.marks(),
