503cac8bb86f771d8d419473a27a9bda0a1baa89
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-12-25 13:59:03 +0300

    remove(langmapper): buggy plugin, breaks all mappings, including cmp

--- plugins/user.lua
+++ plugins/user.lua
@@ -141,10 +141,4 @@ return {
       "SnipLive",
     },
   },
-  {
-    "Wansmer/langmapper.nvim",
-    lazy = false,
-    priority = 1, -- High priority is needed if you will use `autoremap()`
-    opts = {},
-  },
 }
