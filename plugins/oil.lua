937be0a4755f90e8e595d5a828bfca22d031f8c7
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-12-25 10:34:42 +0300

    fix(oil): use `trash` only if available

--- plugins/oil.lua
+++ plugins/oil.lua
@@ -34,7 +34,7 @@ return {
   end,
   opts = {
     skip_confirm_for_simple_edits = true,
-    delete_to_trash = true,
+    delete_to_trash = vim.fn.executable("trash") == 1,
     trash_command = "trash",
     view_options = {
       show_hidden = true,
