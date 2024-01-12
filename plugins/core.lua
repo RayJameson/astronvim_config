2adc110e363d7bcb351b22390ab5cb07708ca642
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-01-12 20:23:23 +0300

    feat(luasnip): extend filetypes

--- plugins/core.lua
+++ plugins/core.lua
@@ -24,6 +24,23 @@ return {
   {
     "L3MON4D3/LuaSnip",
     cond = not vim.g.vscode,
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function(_, opts)
+          if opts then require("luasnip").config.setup(opts) end
+          vim.tbl_map(
+            function(type) require("luasnip.loaders.from_" .. type).lazy_load() end,
+            { "vscode", "snipmate", "lua" }
+          )
+          require("luasnip").filetype_extend("lua", { "luadoc" })
+          require("luasnip").filetype_extend("python", { "pydoc" })
+          require("luasnip").filetype_extend("rust", { "rustdoc" })
+          require("luasnip").filetype_extend("typescript", { "tsdoc" })
+          require("luasnip").filetype_extend("javascript", { "jsdoc" })
+        end,
+      },
+    },
     config = function(plugin, opts)
       require("plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
