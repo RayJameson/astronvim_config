46870920b6619cd56909ce312c9fd110b0906664
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-01-23 19:24:08 +0300

    feat(plugins): added mappings to copy to clipboard for openingh.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -115,8 +115,11 @@ return {
       local prefix = "<leader>g"
       return {
         { prefix .. "o", "<cmd>OpenInGHRepo<CR>", desc = "Open git repo in web", mode = { "n" } },
+        { prefix .. "O", "<cmd>OpenInGHRepo+<CR>", desc = "Copy git repo url", mode = { "n" } },
         { prefix .. "f", "<cmd>OpenInGHFile<CR>", desc = "Open git file in web", mode = { "n" } },
+        { prefix .. "F", "<cmd>OpenInGHFile+<CR>", desc = "Copy git file url", mode = { "n" } },
         { prefix .. "f", "<cmd>OpenInGHFileLines<CR>", desc = "Open git lines in web", mode = { "x" } },
+        { prefix .. "F", "<cmd>OpenInGHFileLines+<CR>", desc = "Copy git lines url", mode = { "x" } },
       }
     end,
   },
