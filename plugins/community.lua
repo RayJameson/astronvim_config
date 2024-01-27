f9eafd5e17400210a399cfa27470c5fefed33272
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-01-27 10:27:47 +0300

    remove(community): removed text mode indicatior import (duplicate)

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,7 +7,6 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
-  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.go" },
   {
