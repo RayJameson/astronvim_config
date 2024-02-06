538708665cb3997bea93081d811c9959983394ee
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-02-06 09:00:26 +0300

    feat(plugins): added gitlinker.nvim as replacement of openingh.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -107,4 +107,17 @@ return {
       },
     },
   },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cond = not vim.g.vscode,
+    opts = {},
+    cmd = "GitLink",
+    keys = function()
+      local prefix = "<leader>g"
+      return {
+        { prefix .. "o", "<cmd>GitLink!<CR>", desc = "Open git line(s) in web", mode = { "n", "x" } },
+        { prefix .. "O", "<cmd>GitLink<CR>", desc = "Copy git line(s) url", mode = { "n", "x" } },
+      }
+    end,
+  },
 }
