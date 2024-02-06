143d746ecb33a6b1a2cb071382b3fc1d72c42252
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-02-06 09:00:05 +0300

    remove(openingh): buggy line open

--- plugins/user.lua
+++ plugins/user.lua
@@ -107,20 +107,4 @@ return {
       },
     },
   },
-  {
-    "almo7aya/openingh.nvim",
-    cond = not vim.g.vscode,
-    cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines" },
-    keys = function()
-      local prefix = "<leader>g"
-      return {
-        { prefix .. "o", "<cmd>OpenInGHRepo<CR>", desc = "Open git repo in web", mode = { "n" } },
-        { prefix .. "O", "<cmd>OpenInGHRepo+<CR>", desc = "Copy git repo url", mode = { "n" } },
-        { prefix .. "f", "<cmd>OpenInGHFile<CR>", desc = "Open git file in web", mode = { "n" } },
-        { prefix .. "F", "<cmd>OpenInGHFile+<CR>", desc = "Copy git file url", mode = { "n" } },
-        { prefix .. "f", "<cmd>OpenInGHFileLines<CR>", desc = "Open git lines in web", mode = { "x" } },
-        { prefix .. "F", "<cmd>OpenInGHFileLines+<CR>", desc = "Copy git lines url", mode = { "x" } },
-      }
-    end,
-  },
 }
