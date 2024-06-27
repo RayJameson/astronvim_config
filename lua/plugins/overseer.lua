0eda3695ff9d72f96b251f376f01d3c0cb5bb747
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-06-27 15:13:07 +0300

    refactor(overseer): remove unused keymaps +
    also use "fulscreen" mode

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -21,47 +21,13 @@ return {
     dependencies = {
       "AstroNvim/astrocore",
       opts = {
-        commands = {
-          AutoCompile = {
-            function()
-              require("overseer").run_template({ name = "compile with compiler" }, function(task)
-                if task then
-                  task:add_component { "restart_on_save", paths = { vim.fn.expand("%:p") } }
-                else
-                  vim.notify("Error setting up auto compilation", vim.log.levels.ERROR)
-                end
-              end)
-            end,
-            desc = "Automatically compile the current file with `compiler` on save",
-          },
-          Compile = {
-            function() require("overseer").run_template { name = "compile with compiler" } end,
-            desc = "Compile the current file with `compiler`",
-          },
-          OpOut = {
-            function() require("overseer").run_template { name = "view file output" } end,
-            desc = "View the current file ouptut with `opout`",
-          },
-          Present = {
-            function()
-              require("overseer").run_template({ name = "present with pdfpc" }, function(task)
-                if not task then vim.notify("Unable to start presentation", vim.log.levels.ERROR) end
-              end)
-            end,
-            desc = "Present the current file with `pdfpc`",
-          },
-        },
         mappings = {
           n = {
             [prefix] = { desc = "󱁤 Compilation" },
             [prefix .. "a"] = { "<Cmd>OverseerQuickAction<CR>", desc = "Quick Action" },
             [prefix .. "i"] = { "<Cmd>OverseerInfo<CR>", desc = "Overseer Info" },
-            [prefix .. "c"] = { "<Cmd>Compile<CR>", desc = "Compile" },
-            [prefix .. "C"] = { "<Cmd>AutoCompile<CR>", desc = "Auto Compile" },
             [prefix .. "<CR>"] = { "<Cmd>OverseerToggle<CR>", desc = "Overseer" },
-            [prefix .. "p"] = { "<Cmd>Present<CR>", desc = "Present file output" },
             [prefix .. "r"] = { "<Cmd>OverseerRun<CR>", desc = "Run" },
-            [prefix .. "v"] = { "<Cmd>OpOut<CR>", desc = "View Output" },
           },
         },
       },
@@ -70,12 +36,15 @@ return {
       setup = {
         task_list = {
           strategy = "toggleterm",
-          direction = "left",
+          direction = "bottom",
+          max_height = { 100, 0.99 },
+          height = 100,
           bindings = {
             ["<C-l>"] = false,
             ["<C-h>"] = false,
             ["<C-k>"] = false,
             ["<C-j>"] = false,
+            ["p"] = false,
             q = "<Cmd>close<CR>",
             K = "IncreaseDetail",
             J = "DecreaseDetail",
