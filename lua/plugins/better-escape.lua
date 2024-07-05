0baba3962bf7e9549991ea6a88c7e1844f159a8f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-07-05 15:23:16 +0300

    feat(better-escape): upstream fixed mapping issue
    max397574/better-escape.nvim#63
    max397574/better-escape.nvim#66

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,14 +2,43 @@
 return {
   "max397574/better-escape.nvim",
   cond = not vim.g.vscode,
-  version = "1.0.0",
   opts = function(_, opts)
-    opts.mapping = { "JK", "JJ", "jk", "jj" }
-    opts.keys = function()
+    local esc_fn = function()
       if vim.bo.filetype == "OverseerForm" then
         return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>lx" or "<esc>x"
       end
       return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>"
     end
+    opts.mappings = {
+      i = {
+        j = {
+          -- These can all also be functions
+          k = esc_fn,
+          j = esc_fn,
+        },
+      },
+      c = {
+        j = {
+          k = esc_fn,
+          j = esc_fn,
+        },
+      },
+      t = {
+        j = {
+          k = esc_fn,
+          j = esc_fn,
+        },
+      },
+      v = {
+        j = {
+          k = esc_fn,
+        },
+      },
+      s = {
+        j = {
+          k = esc_fn,
+        },
+      },
+    }
   end,
 }
