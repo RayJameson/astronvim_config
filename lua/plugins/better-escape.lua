6d1151bd6ed60d150aa38df485fb66d476094553
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-07-05 15:10:30 +0300

    feat(better-escape): pin version and fix esc in overseer run

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,8 +2,14 @@
 return {
   "max397574/better-escape.nvim",
   cond = not vim.g.vscode,
+  version = "1.0.0",
   opts = function(_, opts)
     opts.mapping = { "JK", "JJ", "jk", "jj" }
-    opts.keys = function() return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>" end
+    opts.keys = function()
+      if vim.bo.filetype == "OverseerForm" then
+        return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>lx" or "<esc>x"
+      end
+      return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>"
+    end
   end,
 }
