f8b51e09b9e7a7b3013f8a02174b1b98384738a2
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-07-08 22:05:16 +0300

    feat(oil): update plugin config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,27 +5,52 @@ return {
     "nvim-tree/nvim-web-devicons",
     {
       "rebelot/heirline.nvim",
+      optional = true,
+      dependencies = { "AstroNvim/astroui", opts = { status = { winbar = { enabled = { filetype = { "^oil$" } } } } } },
       opts = function(_, opts)
-        local status = require("astroui.status")
-        local is_oil = function(bufnr) return status.condition.buffer_matches({ filetype = "^oil$" }, bufnr) end
-        local disable_winbar_cb = opts.opts.disable_winbar_cb
-        opts.opts.disable_winbar_cb = function(args)
-          if is_oil(args.buf) then return false end
-          return disable_winbar_cb(args)
-        end
-
         if opts.winbar then
+          local status = require("astroui.status")
           table.insert(opts.winbar, 1, {
-            condition = function(self) return is_oil(self.bufnr) end,
+            condition = function(self) return status.condition.buffer_matches({ filetype = "^oil$" }, self.bufnr) end,
             status.component.separated_path {
               padding = { left = 2 },
               max_depth = false,
               suffix = false,
-              path_func = function() return require("oil").get_current_dir() end,
+              path_func = function(self) return require("oil").get_current_dir(self.bufnr) end,
             },
           })
         end
       end,
+    },
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          oil_settings = {
+            {
+              event = "FileType",
+              desc = "Disable view saving for oil buffers",
+              pattern = "oil",
+              callback = function(args) vim.b[args.buf].view_activated = false end,
+            },
+            {
+              event = "User",
+              pattern = "OilActionsPost",
+              desc = "Close buffers when files are deleted in Oil",
+              callback = function(args)
+                if args.data.err then return end
+                for _, action in ipairs(args.data.actions) do
+                  if action.type == "delete" then
+                    local _, path = require("oil.util").parse_url(action.url)
+                    local bufnr = vim.fn.bufnr(path)
+                    if bufnr ~= -1 then require("astrocore.buffer").wipe(bufnr, true) end
+                  end
+                end
+              end,
+            },
+          },
+        },
+      },
     },
   },
   keys = {
@@ -61,30 +86,6 @@ return {
         end,
       })
     end
-    vim.api.nvim_create_augroup("oil_settings", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      group = "oil_settings",
-      pattern = "oil",
-      desc = "Disable view saving for oil buffers",
-      callback = function(args) vim.b[args.buf].view_activated = false end,
-    })
-    vim.api.nvim_create_autocmd("User", {
-      group = "oil_settings",
-      pattern = "OilActionsPost",
-      desc = "Logic to run after an action in Oil",
-      callback = function(args)
-        if args.data.err then return end
-        for _, action in ipairs(args.data.actions) do
-          ---@cast action oil.Action
-          if action.type == "delete" then
-            ---@cast action oil.DeleteAction
-            local _, path = require("oil.util").parse_url(action.url)
-            local bufnr = vim.fn.bufnr(path)
-            if bufnr ~= -1 then require("astrocore.buffer").wipe(bufnr, true) end
-          end
-        end
-      end,
-    })
   end,
   opts = {
     skip_confirm_for_simple_edits = true,
