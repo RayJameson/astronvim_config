fb28267f265616dcd24cf031e1d357322c53b4ba
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-07-08 16:32:50 +0300

    feat(community): added nvim-highlight-colors

--- lua/community.lua
+++ lua/community.lua
@@ -61,4 +61,5 @@ return {
   },
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.color.nvim-highlight-colors" },
 }
