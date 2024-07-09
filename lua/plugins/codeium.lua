fc2a9a87a2697f208d3ebcc2bc104ff20f263c54
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-07-09 17:09:17 +0300

    refactor(codeium): use `specs` instead of `config`

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,20 +3,45 @@ return {
   "Exafunction/codeium.vim",
   cond = not vim.g.vscode,
   event = "LspAttach",
-  config = function()
-      -- stylua: ignore start
-      vim.keymap.set("i", "<C-g>", function() return vim.api.nvim_call_function("codeium#Accept", {}) end, { expr = true })
-      vim.keymap.set("i", "<C-;>", function() return vim.api.nvim_call_function("codeium#CycleCompletions", {1}) end, { expr = true })
-      vim.keymap.set("i", "<C-,>", function() return vim.api.nvim_call_function("codeium#CycleCompletions", {-1}) end, { expr = true })
-      vim.keymap.set("i", "<C-x>", function() return vim.api.nvim_call_function("codeium#Clear", {}) end, { expr = true })
-    -- stylua: ignore end
-    vim.keymap.set("n", "<Leader>;", function()
-      -- HACK: initially there is no vim.g.codeium_enabled variable even if Codeium enabled
-      if vim.g.codeium_enabled == true or vim.g.codeium_enabled == nil then
-        vim.cmd("CodeiumDisable")
-      else
-        vim.cmd("CodeiumEnable")
-      end
-    end, { noremap = true, desc = "Toggle Codeium" })
-  end,
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>;"] = {
+              function()
+                -- HACK: initially there is no vim.g.codeium_enabled variable even if Codeium enabled
+                if vim.g.codeium_enabled == true or vim.g.codeium_enabled == nil then
+                  vim.cmd("CodeiumDisable")
+                  vim.notify("Codeium disabled")
+                else
+                  vim.cmd("CodeiumEnable")
+                  vim.notify("Codeium enabled")
+                end
+              end,
+              desc = "Toggle Codeium",
+            },
+          },
+          i = {
+            ["<C-g>"] = {
+              function() return vim.api.nvim_call_function("codeium#Accept", {}) end,
+              desc = "Codeium accept",
+              expr = true,
+            },
+            ["<C-,"] = {
+              function() return vim.api.nvim_call_function("codeium#CycleCompletions", { 1 }) end,
+              desc = "Codeium next",
+              expr = true,
+            },
+            ["<C-x>"] = {
+              function() return vim.api.nvim_call_function("codeium#Clear", {}) end,
+              desc = "Codeium clear",
+              expr = true,
+            },
+          },
+        },
+      },
+    },
+  },
 }
