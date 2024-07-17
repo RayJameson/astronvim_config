06c4d14e6758fdda371021ce0b838692cdfa1c86
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-07-17 11:37:43 +0300

    fix(better-escape): finally fix visual mode escape mapping

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,32 +2,39 @@
 return {
   "max397574/better-escape.nvim",
   cond = not vim.g.vscode,
-  opts = function(_, opts)
+  opts = function()
     local esc_fn = function()
       if vim.bo.filetype == "OverseerForm" then
         return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<Esc>lx" or "<Esc>x"
       end
       return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<Esc>l" or "<Esc>"
     end
-    opts.mappings = {
-      i = {
-        j = {
-          k = esc_fn,
+    local disabled = {
+      j = { j = false, k = false },
+      k = { j = false, k = false },
+    }
+    return {
+      create_default_mappings = false,
+      mappings = {
+        i = {
+          j = {
+            k = esc_fn,
+          },
         },
-      },
-      c = {
-        j = {
-          k = esc_fn,
+        c = {
+          j = {
+            k = esc_fn,
+          },
         },
-      },
-      t = {
-        j = {
-          k = "<C-\\><C-n>",
+        t = {
+          j = {
+            k = "<C-\\><C-n>",
+          },
         },
+        v = disabled,
+        x = disabled,
+        s = disabled,
       },
-      v = {},
-      x = {},
-      s = {},
     }
   end,
 }
