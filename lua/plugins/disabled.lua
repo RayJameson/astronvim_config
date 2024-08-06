824552100875e123b052925d6dd38c855389bcbe
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-08-06 13:40:04 +0300

    remove(plugins): disable aerial

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, {
     "s1n7ax/nvim-window-picker",
     "nvim-neo-tree/neo-tree.nvim",
     "stevearc/resession.nvim",
+    "stevearc/aerial.nvim",
 })
