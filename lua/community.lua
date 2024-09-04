c825061ee0877a4019545eb61eb7456d96c154b0
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-04 16:36:51 +0300

    feat(community): added `lsplinks`

--- lua/community.lua
+++ lua/community.lua
@@ -64,4 +64,5 @@ return {
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.color.nvim-highlight-colors" },
+  { import = "astrocommunity.lsp.lsplinks-nvim" },
 }
