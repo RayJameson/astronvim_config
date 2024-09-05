67da88a5eec653fdd5b02601ac2316bf917c10e6
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-06 02:36:44 +0300

    fix(basedpyright): pythonPath for `pyenv`

--- lua/plugins/lsp/config/basedpyright.lua
+++ lua/plugins/lsp/config/basedpyright.lua
@@ -9,31 +9,25 @@ return {
         before_init = function(_, c)
           if not c.settings then c.settings = {} end
           if not c.settings.python then c.settings.python = {} end
-          c.settings.python.pythonPath = vim.fn.exepath("python")
+          c.settings.python.pythonPath = (vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV .. "/bin/python3")
+            or vim.fn.exepath("python3")
         end,
         settings = {
           basedpyright = {
             analysis = {
               diagnosticMode = "workspace",
-              -- diagnosticMode = "openFilesOnly",
               typeCheckingMode = "basic",
               autoImportCompletions = true,
               autoSearchPath = true,
-              -- inlayHints = {
-              --   variableTypes = true,
-              --   functionReturnTypes = true,
-              --   callArgumentNames = true,
-              --   pytestParameters = true,
-              -- },
               useLibraryCodeForTypes = true,
               diagnosticSeverityOverrides = {
                 reportUnusedImport = "information",
                 reportUnusedFunction = "information",
                 reportUnusedVariable = "information",
-                -- reportGeneralTypeIssues = "none",
-                -- reportOptionalMemberAccess = "none",
-                -- reportOptionalSubscript = "none",
-                -- reportPrivateImportUsage = "none",
+                reportGeneralTypeIssues = "none",
+                reportOptionalMemberAccess = "none",
+                reportOptionalSubscript = "none",
+                reportPrivateImportUsage = "none",
               },
             },
           },
