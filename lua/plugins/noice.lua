923fe1714857b902033ded74a7e1e6d3bd5099ec
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-10 15:57:43 +0300

    feat(noice): disable signature render, and md override

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -16,11 +16,7 @@ return {
       lsp = {
         progress = { enabled = false },
         hover = { enabled = false },
-        override = {
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        },
+        signature = { enabled = false },
       },
       routes = {
         {
