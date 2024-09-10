9b58542c848e40808435b1179616f1bad37e0135
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-10 15:58:28 +0300

    feat(disabled): re-enable aerial for breadcrumbs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,5 +4,4 @@ return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, {
     "s1n7ax/nvim-window-picker",
     "nvim-neo-tree/neo-tree.nvim",
     "stevearc/resession.nvim",
-    "stevearc/aerial.nvim",
 })
