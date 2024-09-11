da22c1f9bbcfa4e982700572936fd015dee90a63
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-11 16:00:21 +0300

    feat(dap): remap some mappings

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -6,7 +6,10 @@ return {
     { "AstroNvim/astroui", opts = { icons = { Debugger = "" } } },
     {
       "AstroNvim/astrocore",
-      opts = function(_, opts --[[@as AstroCoreOpts]])
+      opts = function(
+        _,
+        opts --[[@as AstroCoreOpts]]
+      )
         local maps = opts.mappings
         maps.n["<Leader>d"] = { desc = require("astroui").get_icon("Debugger", 1, false) .. "Debugger" }
         maps.n["<F8>"] = { function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" }
@@ -18,6 +21,17 @@ return {
         maps.n["<Leader>dO"] = { function() require("dap").step_out() end, desc = "Step Out (S-F10)" }
         maps.n["<Leader>do"] = { function() require("dap").step_over() end, desc = "Step Over (F9)" }
         maps.n["<Leader>dd"] = { function() require("dap").disconnect() end, desc = "Disconnect" }
+        maps.n["<C-F5>"] = { function() require("dap").restart_frame() end, desc = "Debugger: Restart" } -- Control+F5
+        maps.n["<S-F5>"] = { function() require("dap").terminate() end, desc = "Debugger: Stop" } -- Shift+F5
+        maps.n["<S-F9>"] = { -- Shift+F9
+          function()
+            vim.ui.input({ prompt = "Condition: " }, function(condition)
+              if condition then require("dap").set_breakpoint(condition) end
+            end)
+          end,
+          desc = "Debugger: Conditional Breakpoint",
+        }
+
         maps.n["<F11>"] = false
       end,
     },
