ef72357cfbd9301e8c98e62b03de14ca73d18766
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-12 10:38:20 +0300

    refactor(grapple): use astronvim v4 libs

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -1,22 +1,34 @@
-local prefix = "<Leader><Leader>"
-local maps = { n = {} }
-local icon = vim.g.icons_enabled and "󰛢 " or ""
-maps.n[prefix] = { desc = icon .. "Grapple" }
-require("astrocore").set_mappings(maps)
 ---@type LazySpec
 return {
   "cbochs/grapple.nvim",
   cond = not vim.g.vscode,
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = { "Grapple" },
-  keys = {
-    { prefix .. "t", "<cmd>Grapple toggle<CR>", desc = "Toggle a file" },
-    { prefix .. "e", "<cmd>Grapple toggle_tags<CR>", desc = "Select from tags" },
-    { prefix .. "s", "<cmd>Grapple toggle_scopes<CR>", desc = "Select a project scope" },
-    { "<C-n>", "<cmd>Grapple cycle forward<CR>", desc = "Select next tag" },
-    { "<C-p>", "<cmd>Grapple cycle backward<CR>", desc = "Select previous tag" },
+  opts = function(_, opts) opts.scope = "git_branch" end,
+  specs = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = { icons = { Grapple = "󰛢" } },
+    },
+    {
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        if opts.mappings == nil then opts.mappings = {} end
+        local maps, prefix = opts.mappings, "<Leader><Leader>" ---@cast maps -nil
+        maps.n[prefix] = { desc = require("astroui").get_icon("Grapple", 1, true) .. "Grapple" }
+        maps.n[prefix .. "t"] = {
+          "<cmd>Grapple toggle<CR>",
+          desc = "Toggle a file",
+        }
+        maps.n[prefix .. "s"] = {
+          "<cmd>Grapple toggle_scopes<CR>",
+          desc = "Toggle a file",
+        }
+        maps.n["<C-n>"] = { "<cmd>Grapple cycle forward<CR>", desc = "Select next tag" }
+        maps.n["<C-p>"] = { "<cmd>Grapple cycle backward<CR>", desc = "Select previous tag" }
+      end,
+    },
   },
-  opts = function(_, opts)
-    opts.scope = "git_branch"
-  end
 }
