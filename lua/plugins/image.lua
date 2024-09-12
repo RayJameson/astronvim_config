5bcc2afb98e5eba2771ceda7e1b7b1f74f8e1e9e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-12 10:37:13 +0300

    feat(plugins): added `image.nvim` plugin

---
+++ lua/plugins/image.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+return {
+  "3rd/image.nvim",
+  cond = vim.fn.executable("magick") == 1,
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      optional = true,
+      opts = function(_, opts)
+        if opts.ensure_installed ~= "all" then
+          opts.ensure_installed =
+            require("astrocore").list_insert_unique(opts.ensure_installed, { "markdown", "markdown_inline" })
+        end
+      end,
+    },
+    "https://github.com/leafo/magick",
+  },
+  opts = {
+    backend = "kitty",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      },
+      neorg = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "norg" },
+      },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = nil,
+    max_height_window_percentage = 50,
+    kitty_method = "normal",
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+  },
+}
