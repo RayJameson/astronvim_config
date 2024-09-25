13896b69d1814f879a7474c5a10bf199bc31d0b1
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-25 12:45:43 +0300

    feat(molten): changed mappings

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -9,11 +9,19 @@ return {
       if not opts.mappings then opts.mappings = {} end
       local prefix = "<leader>m"
 
-      opts.mappings.n[prefix] = { "<Cmd>MoltenInit<CR>", desc = "Molten" }
+      opts.mappings.n[prefix] = { desc = "Molten" }
       opts.mappings.n[prefix .. "e"] = { "<Cmd>MoltenEvaluateOperator<CR>", desc = "Run operator selection" }
-      opts.mappings.n[prefix .. "rl"] = { "<Cmd>MoltenEvaluateLine<CR>", desc = "Evaluate line" }
-      opts.mappings.n[prefix .. "rr"] = { "<Cmd>MoltenReevaluateCell<CR>", desc = "Re-evaluate cell" }
-      opts.mappings.v[prefix .. "r"] = { ":<C-u>MoltenEvaluateVisual<CR>gv", desc = "Evaluate visual selection" }
+      opts.mappings.n[prefix .. "l"] = { "<Cmd>MoltenEvaluateLine<CR>", desc = "Evaluate line" }
+      opts.mappings.n[prefix .. "<CR>"] = { "<Cmd>noautocmd MoltenEnterOutput<CR>", desc = "Enter output" }
+      opts.mappings.n[prefix .. "h"] = { "<Cmd>MoltenHideOutput<CR>", desc = "Hide output" }
+      opts.mappings.n[prefix .. "d"] = { "<Cmd>MoltenDelete<CR>", desc = "Delete cell" }
+      opts.mappings.n[prefix .. "c"] = { "<Cmd>MoltenReevaluateCell<CR>", desc = "Re-evaluate cell" }
+
+      opts.mappings.x[prefix] = { desc = "Molten" }
+      opts.mappings.x[prefix .. "r"] = { ":<C-u>MoltenEvaluateVisual<CR>gv", desc = "Evaluate visual selection" }
+
+      if not opts.options then opts.options = {} end
+      opts.options.g.molten_output_virt_lines = true
     end,
   },
 }
