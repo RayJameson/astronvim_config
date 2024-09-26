61701317e62fc531ddcbd0bbdbf87ae2f886130b
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-26 16:10:22 +0300

    fix(image): all images render is buggy

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -23,7 +23,7 @@ return {
         enabled = true,
         clear_in_insert_mode = false,
         download_remote_images = true,
-        only_render_image_at_cursor = false,
+        only_render_image_at_cursor = true,
         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
       },
       neorg = {
