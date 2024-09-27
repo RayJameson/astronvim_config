fd9c63f384dd504c1ccfd3074eeb11044c33f3dc
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-27 15:58:26 +0300

    feat(plugins): added `lsp_lines`

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -14,7 +14,6 @@ return {
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
-      virtual_text = true,
       underline = true,
       update_in_insert = false,
     },
