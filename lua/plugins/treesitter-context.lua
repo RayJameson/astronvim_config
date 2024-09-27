86c4f1f6738cb99fc80d3d65cf7cb2d5ef03c9f5
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-09-27 15:58:59 +0300

    remove(treesitter-context): how this mapping even got there

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,22 +1,6 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter-context",
-  specs = {
-    {
-      "AstroNvim/astrocore",
-      ---@type AstroCoreOpts
-      opts = {
-        mappings = {
-          n = {
-            ["<Leader>uD"] = {
-              function() require("notify").dismiss { silent = true, pending = true } end,
-              desc = "Dismiss notification",
-            },
-          },
-        },
-      },
-    },
-  },
   cond = not vim.g.vscode,
   event = "User Astrofile",
   dependencies = {
