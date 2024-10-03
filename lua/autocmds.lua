b12f97d25a17bdc6c83572dc07dcc8e517e08dcd
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-03 22:51:44 +0300

    chore(autocmds): added comment about `markdown` help files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -103,6 +103,7 @@ for file_extension, command in pairs {
       })
     end,
   })
+  -- INFO: doesn't work if help file has `markdown` filetype modeline
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "help",
     group = vim.api.nvim_create_augroup("HelpPages", {}),
