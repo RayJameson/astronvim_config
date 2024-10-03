20507dda909e858ace2e7440d0417576ff91d7c2
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-03 22:57:19 +0300

    remove(plugins): removed `treesitter-context`

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,19 +1,6 @@
 ---@type LazySpec
 return {
   "rcarriga/nvim-notify",
-  specs = {
-    {
-      "AstroNvim/astrocore",
-      ---@type AstroCoreOpts
-      opts = {
-        mappings = {
-          n = {
-            ["[c"] = { function() require("treesitter-context").go_to_context() end, desc = "Go to context" },
-          },
-        },
-      },
-    },
-  },
   opts = {
     background_colour = "#000000",
     stages = "fade",
