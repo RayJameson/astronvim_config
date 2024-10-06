f5e1e62263f604e161de5fb7230a5d1d88a726eb
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-06 14:01:49 +0300

    Revert "feat(grapple): use letters instead of digits for quick select"
    This reverts commit d7bcd80f1d79f9e15b7e267be038c0b58b23aacc.

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -3,10 +3,7 @@ return {
   "cbochs/grapple.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = { "Grapple" },
-  opts = function(_, opts)
-    opts.scope = "git_branch"
-    opts.quick_select = "qwertasdf"
-  end,
+  opts = function(_, opts) opts.scope = "git_branch" end,
   specs = {
     {
       "AstroNvim/astroui",
