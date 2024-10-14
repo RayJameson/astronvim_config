2f79003ddb20727bb18c8bcb4dc8b46b319556b4
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-14 18:52:02 +0300

    feat(oil): added mapping for `tcd`

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -114,7 +114,8 @@ return {
           vim.fn.setreg("+", dir .. entry.name)
         end,
       },
-      [","] = { "actions.cd", opts = { scope = "win" }, desc = ":lcd to the current oil directory" },
+      [","] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+      ["."] = { "actions.cd", opts = { scope = "win" }, desc = ":lcd to the current oil directory" },
     },
   },
 }
