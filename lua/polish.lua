b6d07b47d4ed6473f4cd1202e9254d7653f26118
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-14 18:52:46 +0300

    fix(neovide): wrong variable value

--- lua/polish.lua
+++ lua/polish.lua
@@ -57,7 +57,7 @@ require("autocmds")
 
 if vim.g.neovide then
   if jit.os == "OSX" then
-    vim.g.neovide_input_macos_alt_is_meta = true
+    vim.g.neovide_input_macos_alt_is_meta = "both"
     vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
     vim.keymap.set("v", "<D-c>", '"+y') -- Copy
     vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
