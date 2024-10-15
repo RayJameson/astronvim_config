61893476fe5d1363985e84b1e929cb2c1e59f83a
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-15 14:09:43 +0300

    fix(neovide): wrong clipboard mappings for macos

--- lua/polish.lua
+++ lua/polish.lua
@@ -59,12 +59,11 @@ if vim.g.neovide then
     vim.g.neovide_input_macos_option_key_is_meta = "both"
     vim.g.neovide_scale_factor = 1.6
     vim.o.guifont = "Iosevka Nerd Font Mono Condensed ExtraLight:h16"
-    vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
     vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-    vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-    vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-    vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-    vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+    vim.keymap.set("n", "<D-v>", '"+p') -- Paste normal mode
+    vim.keymap.set("v", "<D-v>", '"+p') -- Paste visual mode
+    vim.keymap.set("c", "<D-v>", "<C-r>+") -- Paste command mode
+    vim.keymap.set("i", "<D-v>", '<C-r>+"') -- Paste insert mode
   else
     vim.o.guifont = "Iosevka Nerd Font Mono SemiCondensed Light:h16"
     vim.keymap.set("x", "<C-S-C>", '"+y', { desc = "Copy to system clipboard" })
