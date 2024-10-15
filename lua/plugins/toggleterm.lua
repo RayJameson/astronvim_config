ea35e6ac2a131c97fd5a401e121a0364bad30d31
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-15 12:21:12 +0300

    feat(toggleterm): adaptive float term size, take up 95% of screen

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,14 @@
+---Calculate percent of base number
+---@param base_number integer | number should be positive number
+---@param percents_of_base integer
+---@return integer
+local function calculate_percent(base_number, percents_of_base)
+  assert(base_number > 0, "Base number in `calculate_percent` should be positive")
+  local _1_percent = base_number / 100
+  local result = math.floor(percents_of_base * _1_percent)
+  return result
+end
+
 ---@type LazySpec
 return {
   "akinsho/toggleterm.nvim",
@@ -5,5 +16,9 @@ return {
   opts = {
     direction = "float",
     autochdir = true,
+    float_opts = {
+      width = calculate_percent(vim.api.nvim_win_get_width(0), 95),
+      height = calculate_percent(vim.api.nvim_win_get_height(0), 95),
+    },
   },
 }
