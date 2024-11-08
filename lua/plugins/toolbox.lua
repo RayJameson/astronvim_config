457d6b05fe1780312f6470e37306f198cf3d408e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-09 03:41:55 +0500

    feat(plugins): added `toolbox.nvim`
    this plugin makes niche commands easier

---
+++ lua/plugins/toolbox.lua
@@ -0,0 +1,58 @@
+return {
+  "DanWlker/toolbox.nvim",
+  specs = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = { icons = { Toolbox = "󱁤" } },
+    },
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        return require("astrocore").extend_tbl(opts, {
+          mappings = {
+            n = {
+              ["<Leader>T"] = {
+                function() require("toolbox").show_picker() end,
+                desc = require("astroui").get_icon("Toolbox", 1, true) .. "Toolbox",
+              },
+            },
+            x = {
+              ["<Leader>T"] = {
+                function() require("toolbox").show_picker() end,
+                desc = require("astroui").get_icon("Toolbox", 1, true) .. "Toolbox",
+              },
+            },
+          },
+        })
+      end,
+    },
+  },
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    commands = {
+      {
+        name = "Format Json",
+        execute = "%!jq '.'",
+      },
+      {
+        name = "Print Lua table",
+        execute = function(v) print(vim.inspect(v)) end,
+      },
+      {
+        name = "Copy relative path to clipboard",
+        execute = function()
+          local path = vim.fn.expand("%")
+          vim.fn.setreg("+", path)
+        end,
+      },
+      {
+        name = "Copy absolute path to clipboard",
+        execute = function()
+          local path = vim.fn.expand("%:p")
+          vim.fn.setreg("+", path)
+        end,
+      },
+    },
+  },
+}
